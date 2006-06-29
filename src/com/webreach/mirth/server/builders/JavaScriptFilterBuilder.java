cb3386f6e3ea0df4cc49ae4bc36aeeaa22380fda
Gerald Bortis <gbortis@nextgen.com>
2006-06-29 21:12:45 +0000

    Filter script is set to "return true" if no rules are set.

--- src/com/webreach/mirth/server/builders/JavaScriptFilterBuilder.java
+++ src/com/webreach/mirth/server/builders/JavaScriptFilterBuilder.java
@@ -8,14 +8,22 @@ import com.webreach.mirth.model.Filter;
 public class JavaScriptFilterBuilder {
 	public String getScript(Filter filter) throws BuilderException {
 		StringBuilder builder = new StringBuilder();
-		builder.append("return (");
+		builder.append("return ");
 
-		for (Iterator iter = filter.getRules().iterator(); iter.hasNext();) {
-			Rule rule = (Rule) iter.next();
-			builder.append(rule.getScript() + " ");
+		if (filter.getRules().isEmpty()) {
+			builder.append("true");
+		} else {
+			builder.append("(");
+			
+			for (Iterator iter = filter.getRules().iterator(); iter.hasNext();) {
+				Rule rule = (Rule) iter.next();
+				builder.append(rule.getScript() + " ");
+			}
+			
+			builder.append(")");
 		}
-
-		builder.append(");");
+		
+		builder.append(";");
 		return builder.toString();
 	}
 }
