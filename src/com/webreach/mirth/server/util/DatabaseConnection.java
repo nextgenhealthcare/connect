12bfaaaed4d70fa6a9703bf8903487c324ad4fe8
Gerald Bortis <gbortis@nextgen.com>
2006-07-07 21:57:32 +0000

    Removed unused import.

--- src/com/webreach/mirth/server/util/DatabaseConnection.java
+++ src/com/webreach/mirth/server/util/DatabaseConnection.java
@@ -32,7 +32,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.Properties;
 
