86959dc8ee351621908c394bdc8e59090d54f790
Gerald Bortis <gbortis@nextgen.com>
2006-07-07 22:24:04 +0000

    Server runs RESTART rather than START on Mule cofiguration exception.

--- src/com/webreach/mirth/server/Mirth.java
+++ src/com/webreach/mirth/server/Mirth.java
@@ -113,7 +113,7 @@ public class Mirth {
 			systemLogger.logSystemEvent(event);
 			
 			configurationController.deleteLatestConfiguration();
-			commandQueue.addCommand(new Command(Command.Operation.START));
+			commandQueue.addCommand(new Command(Command.Operation.RESTART));
 		} catch (ControllerException e) {
 			logger.warn("Could not retrieve latest configuration.", e);
 		}
