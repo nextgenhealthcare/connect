bddc33520de8c5711c52e96689c5f8cfbd1b054b
Gerald Bortis <gbortis@nextgen.com>
2006-07-11 18:48:28 +0000

    Removed StopMirth.java

--- src/com/webreach/mirth/server/StopMirth.java
+++
@@ -1,8 +0,0 @@
-package com.webreach.mirth.server;
-
-public class StopMirth {
-	public static void main(String[] args) {
-		CommandQueue.getInstance().addCommand(new Command(Command.Operation.STOP));
-	}
-
-}
