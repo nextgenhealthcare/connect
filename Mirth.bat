18c7335216f21ebb82888ebd85d500ece8452964
Gerald Bortis <gbortis@nextgen.com>
2006-07-27 23:53:46 +0000

    Added task to build.xml to generate mirth-launcher.jar. Added Mirth.bat since it no longer needs to be generated.

---
+++ Mirth.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+SET CLASSPATH=mirth-launcher.jar
+SET CLASSPATH=%CLASSPATH%;lib/log4j-1.2.13.jar
+SET CLASSPATH=%CLASSPATH%;config/
+java -classpath %CLASSPATH% com.webreach.mirth.server.launcher.Launcher launcher.xml
