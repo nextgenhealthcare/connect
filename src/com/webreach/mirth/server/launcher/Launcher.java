e78d6579caa30bd21079d81f7dfb817eea52e0c6
Gerald Bortis <gbortis@nextgen.com>
2006-07-27 21:47:31 +0000

    Launcher now loads its classpath from launcher.xml using ClasspathBuilder.

--- src/com/webreach/mirth/server/launcher/Launcher.java
+++ src/com/webreach/mirth/server/launcher/Launcher.java
@@ -4,15 +4,20 @@ import java.net.URLClassLoader;
 
 public class Launcher {
 	public static void main(String[] args) {
-		try {
-			CustomLibrary library = new CustomLibrary(args);
-			URLClassLoader classLoader = new URLClassLoader(library.getLibrary());
-			Class mirthClass = classLoader.loadClass("com.webreach.mirth.server.Mirth");
-			Thread mirthThread = (Thread) mirthClass.newInstance();
-			mirthThread.setContextClassLoader(classLoader);
-			mirthThread.start();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (args[0] != null) {
+			try {
+				ClasspathBuilder builder = new ClasspathBuilder(args[0]);
+				URLClassLoader classLoader = new URLClassLoader(builder.getClasspath());
+				Class mirthClass = classLoader.loadClass("com.webreach.mirth.server.Mirth");
+				Thread mirthThread = (Thread) mirthClass.newInstance();
+				mirthThread.setContextClassLoader(classLoader);
+				mirthThread.start();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("usage: java Launcher launcher.xml");
 		}
+		
 	}
 }
