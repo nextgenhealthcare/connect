af50fc23da37324626cb8de338fb96ee0f9be391
Gerald Bortis <gbortis@nextgen.com>
2006-08-01 22:56:23 +0000

    Removed OutboundChannel since it was redundant and renamed InboundChannel to ChannelComponent.

--- src/com/webreach/mirth/server/mule/components/ChannelComponent.java
+++ src/com/webreach/mirth/server/mule/components/ChannelComponent.java
@@ -6,15 +6,7 @@ import org.apache.log4j.Logger;
 import org.mule.umo.UMOEventContext;
 import org.mule.umo.lifecycle.Callable;
 
-import ca.uhn.hl7v2.model.Message;
-import ca.uhn.hl7v2.parser.PipeParser;
-import ca.uhn.hl7v2.util.Terser;
-import ca.uhn.hl7v2.validation.impl.NoValidation;
-
-import com.webreach.mirth.model.MessageEvent;
-import com.webreach.mirth.server.controllers.MessageLogger;
-
-public class InboundChannel implements Callable {
+public class ChannelComponent implements Callable {
 	private Logger logger = Logger.getLogger(this.getClass());
 	public static HashMap globalMap = new HashMap();
 	
