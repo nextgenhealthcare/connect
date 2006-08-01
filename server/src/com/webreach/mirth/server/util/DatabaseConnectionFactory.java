f2c26b5b83e6cc0d4bb699d790920706e860e72f
Gerald Bortis <gbortis@nextgen.com>
2006-08-01 22:51:51 +0000

    Removed EmbeddedDatabaseConnection and refactord transformer and filter to use the DatabaseConnectionFactory instead.

--- server/src/com/webreach/mirth/server/util/DatabaseConnectionFactory.java
+++ server/src/com/webreach/mirth/server/util/DatabaseConnectionFactory.java
@@ -30,26 +30,35 @@ import java.util.Properties;
 
 import com.webreach.mirth.util.PropertyLoader;
 
-
 public class DatabaseConnectionFactory {
-	private static boolean isDriverRegistered = false;
-	private static Properties properties = PropertyLoader.loadProperties("mirth");
-	
 	public static DatabaseConnection createDatabaseConnection() throws SQLException {
-		if (!isDriverRegistered) {
-			try {
-				Class.forName(properties.getProperty("database.driver"));
-				isDriverRegistered = true;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		Properties properties = PropertyLoader.loadProperties("mirth");
 		
+		try {
+			Class.forName(properties.getProperty("database.driver"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		Properties info = new Properties();
 		info.setProperty("user", "sa");
 		info.setProperty("password", "");
 		info.setProperty("shutdown", "true");
 
 		return new DatabaseConnection(properties.getProperty("database.url"), info);
+	}
+
+	public static DatabaseConnection createDatabaseConnection(String driver, String address, String username, String password) throws SQLException {
+		try {
+			Class.forName(driver);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Properties info = new Properties();
+		info.setProperty("user", username);
+		info.setProperty("password", password);
+
+		return new DatabaseConnection(address, info);
 	}
 }
