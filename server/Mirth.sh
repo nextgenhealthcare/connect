7194e9581e472954a0141c5c4b9952a402fb2c70
Gerald Bortis <gbortis@nextgen.com>
2006-08-02 18:54:33 +0000

    Renamed config to conf. Removed removed DatabaseConnectionFactory and DatabaseConnection from transformer and filter.

--- server/Mirth.sh
+++ server/Mirth.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 CLASSPATH=mirth-launcher.jar
 CLASSPATH=$CLASSPATH:lib/log4j-1.2.13.jar
-CLASSPATH=$CLASSPATH:config/
+CLASSPATH=$CLASSPATH:conf/
 java -classpath $CLASSPATH com.webreach.mirth.server.launcher.Launcher launcher.xml
