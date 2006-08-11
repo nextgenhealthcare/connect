babdeb7ccc2eff428215e9cb01e4ca6e75eb0000
Chris Lang <clang@nextgen.com>
2006-08-11 23:56:25 +0000

    Fixed SMTP Connection error

--- client/src/com/webreach/mirth/client/ui/FunctionListBuilder.java
+++ client/src/com/webreach/mirth/client/ui/FunctionListBuilder.java
@@ -12,7 +12,7 @@ public class FunctionListBuilder {
 		variablelistItems.add(new FunctionListItem("Incoming Message (XML)", "The original incoming ER7 or XML string as XML.", "msg['']"));
 		variablelistItems.add(new FunctionListItem("Log a Debug Statement", "Outputs the message to the system debug log.", "logger.debug('message');"));
 		variablelistItems.add(new FunctionListItem("Log an Error Statement", "Outputs the message to the system error log.", "logger.error('message');"));
-		variablelistItems.add(new FunctionListItem("Send an Email", "Sends an alert email using the alert SMTP properties.", "var smtpConn = SMTPConnection.SMTPConnection();\nsmtpConn.send('to', 'cc', 'from', 'subject', 'body');"));
+		variablelistItems.add(new FunctionListItem("Send an Email", "Sends an alert email using the alert SMTP properties.", "var smtpConn = SMTPConnectionFactory.createSMTPConnection();\nsmtpConn.send('to', 'cc', 'from', 'subject', 'body');"));
 		variablelistItems.add(new FunctionListItem("Perform Database Query", "Performs a database query and returns the resultset.", "var dbConn = DatabaseConnectionFactory.createDatabaseConnection('driver', 'address', 'username', 'password');\nvar result = dbConn.executeQuery('expression');\ndbConn.close();"));
 		variablelistItems.add(new FunctionListItem("Perform Database Update", "Performs a database update.", "var conn = DatabaseConnectionFactory.createDatabaseConnection('driver', 'address', 'username', 'password');\nvar result = conn.executeUpdate('expression');\nconn.close();"));
 		return variablelistItems;
