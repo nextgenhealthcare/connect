a54e02217c84701675841b4d7f173c36efc5319f
Chris Lang <clang@nextgen.com>
2006-08-12 00:26:17 +0000

    Fixed logging issues in JSFilter/Transformer

--- client/src/com/webreach/mirth/client/ui/FunctionListBuilder.java
+++ client/src/com/webreach/mirth/client/ui/FunctionListBuilder.java
@@ -10,10 +10,10 @@ public class FunctionListBuilder {
 		variablelistItems.add(new FunctionListItem("Global Variable Map", "The global variable map that persists values between channels.", "globalMap.get('')"));
 		variablelistItems.add(new FunctionListItem("Incoming Message", "The original incoming ER7 or XML string as received.", "incomingMessage"));
 		variablelistItems.add(new FunctionListItem("Incoming Message (XML)", "The original incoming ER7 or XML string as XML.", "msg['']"));
-		variablelistItems.add(new FunctionListItem("Log a Debug Statement", "Outputs the message to the system debug log.", "logger.debug('message');"));
+		variablelistItems.add(new FunctionListItem("Log an Info Statement", "Outputs the message to the system info log.", "logger.info('message');"));
 		variablelistItems.add(new FunctionListItem("Log an Error Statement", "Outputs the message to the system error log.", "logger.error('message');"));
 		variablelistItems.add(new FunctionListItem("Send an Email", "Sends an alert email using the alert SMTP properties.", "var smtpConn = SMTPConnectionFactory.createSMTPConnection();\nsmtpConn.send('to', 'cc', 'from', 'subject', 'body');"));
-		variablelistItems.add(new FunctionListItem("Perform Database Query", "Performs a database query and returns the resultset.", "var dbConn = DatabaseConnectionFactory.createDatabaseConnection('driver', 'address', 'username', 'password');\nvar result = dbConn.executeQuery('expression');\ndbConn.close();"));
+		variablelistItems.add(new FunctionListItem("Perform Database Query", "Performs a database query and returns the rowset.", "var dbConn = DatabaseConnectionFactory.createDatabaseConnection('driver', 'address', 'username', 'password');\nvar result = dbConn.executeCachedQuery('expression');\ndbConn.close();"));
 		variablelistItems.add(new FunctionListItem("Perform Database Update", "Performs a database update.", "var conn = DatabaseConnectionFactory.createDatabaseConnection('driver', 'address', 'username', 'password');\nvar result = conn.executeUpdate('expression');\nconn.close();"));
 		return variablelistItems;
 	}
