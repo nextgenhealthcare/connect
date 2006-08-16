6053558c455aaf9e7355bb8bbaed7b585fa05e81
Gerald Bortis <gbortis@nextgen.com>
2006-08-16 17:35:33 +0000

    Removed code for updating channel statistics.

--- src/com/webreach/mirth/server/mule/components/ChannelComponent.java
+++ src/com/webreach/mirth/server/mule/components/ChannelComponent.java
@@ -6,16 +6,11 @@ import org.apache.log4j.Logger;
 import org.mule.umo.UMOEventContext;
 import org.mule.umo.lifecycle.Callable;
 
-import com.webreach.mirth.server.controllers.ChannelStatisticsController;
-
 public class ChannelComponent implements Callable {
 	private Logger logger = Logger.getLogger(this.getClass());
 	public static HashMap globalMap = new HashMap();
-	private ChannelStatisticsController statisticsController = new ChannelStatisticsController();
 	
 	public Object onCall(UMOEventContext eventContext) throws Exception {
-		int channelId = Integer.parseInt(eventContext.getComponentDescriptor().getName());
-		statisticsController.updateStatistics(channelId);
 		return eventContext.getTransformedMessage();
 	}
 
