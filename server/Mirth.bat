6373f3b600ba20b48a46b60c75a37a45597fb621
Gerald Bortis <gbortis@nextgen.com>
2006-08-24 21:31:24 +0000

    No longer needs a CLASSPATH.

--- server/Mirth.bat
+++ server/Mirth.bat
@@ -1,5 +1,2 @@
 @ECHO OFF
-SET CLASSPATH=mirth-launcher.jar
-SET CLASSPATH=%CLASSPATH%;lib/log4j-1.2.13.jar
-SET CLASSPATH=%CLASSPATH%;conf/
-java -classpath %CLASSPATH% com.webreach.mirth.server.launcher.Launcher launcher.xml
+java -jar mirth-launcher.jar launcher.xml
