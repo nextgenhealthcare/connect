ca03168bd6ffd417123b57883843023bebae3f6b
Gerald Bortis <gbortis@nextgen.com>
2006-09-05 18:32:03 +0000

    Added GlobalVariableStore.java

---
+++ server/src/com/webreach/mirth/server/mule/util/GlobalVariableStore.java
@@ -0,0 +1,31 @@
+package com.webreach.mirth.server.mule.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class GlobalVariableStore {
+	public Map<String, String> globalVariableMap = new HashMap<String, String>();
+	private static GlobalVariableStore instance = null;
+
+	private GlobalVariableStore() {
+		
+	}
+
+	public static GlobalVariableStore getInstance() {
+		synchronized (GlobalVariableStore.class) {
+			if (instance == null)
+				instance = new GlobalVariableStore();
+
+			return instance;
+		}
+	}
+	
+	public void put(String key, String value) {
+		globalVariableMap.put(key, value);
+	}
+	
+	public String get(String key) {
+		return globalVariableMap.get(key);
+	}
+}
