53bea67040cb17a5e90053bfd884bcbe0ad536d8
Gerald Bortis <gbortis@nextgen.com>
2006-09-06 00:48:19 +0000

    MIRTH-2: Changes containsKey to use String rather than Object.

--- server/src/com/webreach/mirth/server/mule/util/GlobalVariableStore.java
+++ server/src/com/webreach/mirth/server/mule/util/GlobalVariableStore.java
@@ -3,13 +3,12 @@ package com.webreach.mirth.server.mule.util;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class GlobalVariableStore {
 	public Map<String, String> globalVariableMap = new HashMap<String, String>();
 	private static GlobalVariableStore instance = null;
 
 	private GlobalVariableStore() {
-		
+
 	}
 
 	public static GlobalVariableStore getInstance() {
@@ -20,14 +19,15 @@ public class GlobalVariableStore {
 			return instance;
 		}
 	}
-	
-	public boolean containsKey(Object key){
+
+	public boolean containsKey(String key) {
 		return globalVariableMap.containsKey(key);
 	}
+
 	public void put(String key, String value) {
 		globalVariableMap.put(key, value);
 	}
-	
+
 	public String get(String key) {
 		return globalVariableMap.get(key);
 	}
