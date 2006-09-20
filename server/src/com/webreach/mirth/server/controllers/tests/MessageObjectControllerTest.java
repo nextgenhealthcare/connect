a21ba51abbd9b22aa4d2a33330499530a94c0856
Gerald Bortis <gbortis@nextgen.com>
2006-09-20 18:43:07 +0000

    Added controller tests.

---
+++ server/src/com/webreach/mirth/server/controllers/tests/MessageObjectControllerTest.java
@@ -0,0 +1,139 @@
+package com.webreach.mirth.server.controllers.tests;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.webreach.mirth.model.MessageObject;
+import com.webreach.mirth.model.filters.MessageObjectFilter;
+import com.webreach.mirth.server.controllers.ConfigurationController;
+import com.webreach.mirth.server.controllers.ControllerException;
+import com.webreach.mirth.server.controllers.MessageObjectController;
+import com.webreach.mirth.server.tools.ScriptRunner;
+
+public class MessageObjectControllerTest extends TestCase {
+	private MessageObjectController messageObjectController = new MessageObjectController();
+	private ConfigurationController configurationController = new ConfigurationController();
+	private List<MessageObject> sampleMessageObjectList;
+	private String channelId;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		// clear all database tables
+		ScriptRunner.runScript("database.sql");
+
+		// initialize the configuration controller to cache encryption key
+		configurationController.initialize();
+
+		sampleMessageObjectList = new ArrayList<MessageObject>();
+		channelId = UUID.randomUUID().toString();
+		
+		for (int i = 0; i < 10; i++) {
+			MessageObject sampleMessageObject = new MessageObject();
+			sampleMessageObject.setId(UUID.randomUUID().toString());
+			sampleMessageObject.setChannelId(channelId);
+			sampleMessageObject.setConnectorName("SampleConnector");
+			sampleMessageObject.setDateCreated(Calendar.getInstance());
+			sampleMessageObject.setEncrypted(false);
+			sampleMessageObject.setStatus(MessageObject.Status.TRANSFORMED);
+
+			sampleMessageObject.setEncodedData("sample encoded data");
+			sampleMessageObject.setEncodedDataProtocol(MessageObject.Protocol.XML);
+
+			sampleMessageObject.setRawData("<raw_data></raw_data>");
+			sampleMessageObject.setRawDataProtocol(MessageObject.Protocol.XML);
+
+			sampleMessageObject.setTransformedData("sample transformed data");
+			sampleMessageObject.setTransformedDataProtocol(MessageObject.Protocol.XML);
+
+			Map<String, String> sampleVariableMap = new HashMap<String, String>();
+			sampleVariableMap.put("testVariable", "testValue");
+
+			sampleMessageObject.setVariableMap(sampleVariableMap);
+			sampleMessageObject.setErrors("invalid header");
+			
+			sampleMessageObjectList.add(sampleMessageObject);
+		}
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testUpdateMessage() throws ControllerException {
+		MessageObject sampleMessageObject = sampleMessageObjectList.get(0);
+		messageObjectController.updateMessage(sampleMessageObject);
+		
+		MessageObjectFilter testFilter = new MessageObjectFilter();
+		testFilter.setId(sampleMessageObject.getId());
+		List<MessageObject> testMessageObjectList = messageObjectController.getMessages(testFilter);
+		MessageObject testMessageObject = testMessageObjectList.get(0);
+
+		Assert.assertEquals(1, testMessageObjectList.size());
+		Assert.assertEquals(sampleMessageObject, testMessageObject);
+	}
+
+	public void testGetMessages() throws ControllerException {
+		insertSampleMessages();
+		
+		MessageObjectFilter testFilter = new MessageObjectFilter();
+		testFilter.setChannelId(channelId);
+		List<MessageObject> testMessageObjectList = messageObjectController.getMessages(testFilter);
+
+		for (Iterator iter = sampleMessageObjectList.iterator(); iter.hasNext();) {
+			MessageObject sampleMessageObject = (MessageObject) iter.next();
+			Assert.assertTrue(testMessageObjectList.contains(sampleMessageObject));
+		}
+	}
+
+	public void testGetMessageCount() throws ControllerException {
+		insertSampleMessages();
+		
+		MessageObjectFilter testFilter = new MessageObjectFilter();
+		testFilter.setChannelId(channelId);
+		List<MessageObject> testMessageObjectList = messageObjectController.getMessages(testFilter);
+		
+		Assert.assertEquals(testMessageObjectList.size(), messageObjectController.getMessageCount(testFilter));
+	}
+
+	public void testRemoveMessages() throws ControllerException {
+		insertSampleMessages();
+
+		MessageObject sampleMessageObject = sampleMessageObjectList.get(0);
+		MessageObjectFilter testRemoveFilter = new MessageObjectFilter();
+		testRemoveFilter.setId(sampleMessageObject.getId());
+		testRemoveFilter.setStatus(MessageObject.Status.TRANSFORMED);
+		messageObjectController.removeMessages(testRemoveFilter);
+
+		MessageObjectFilter testFilter = new MessageObjectFilter();
+		testFilter.setChannelId(channelId);
+		List<MessageObject> testMessageObjectList = messageObjectController.getMessages(testFilter);
+
+		Assert.assertEquals(sampleMessageObjectList.size() - 1, testMessageObjectList.size());
+	}
+
+	public void testClearMessages() throws ControllerException {
+		insertSampleMessages();
+		messageObjectController.clearMessages(channelId);
+
+		MessageObjectFilter testFilter = new MessageObjectFilter();
+		testFilter.setChannelId(channelId);
+		List<MessageObject> testMessageObjectList = messageObjectController.getMessages(testFilter);
+
+		Assert.assertEquals(0, testMessageObjectList.size());
+	}
+	
+	private void insertSampleMessages() {
+		for (Iterator iter = sampleMessageObjectList.iterator(); iter.hasNext();) {
+			MessageObject messageObject = (MessageObject) iter.next();
+			messageObjectController.updateMessage(messageObject);
+		}
+	}
+}
