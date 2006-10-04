7b37a6c17268cb1a5051a933f57364fe6abd4767
Gerald Bortis <gbortis@nextgen.com>
2006-10-04 19:36:51 +0000

    Fixed package names and made minor formatting changes.

--- server/src/org/mule/providers/pdf/BBParser.java
+++ server/src/org/mule/providers/pdf/BBParser.java
@@ -1,112 +1,78 @@
-/*
- * BBParser.java
- *
- * Created on October 4, 2006, 10:39 AM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
+package org.mule.providers.pdf;
 
-package com.webreach.mirth.client.ui.util;
+public class BBParser {
+	private String current = new String();
+	private int currentIndex;
+	private String[] startKeywords = { "", "[i]", "[u]", "[b]" };
+	private String[] endKeywords = { "", "[/i]", "[/u]", "[/b]" };
+	private String[] keywordType = { "NORMAL", "ITALIC", "UNDERLINE", "BOLD" };
 
-/**
- *
- * @author brendanh
- */
-public class BBParser
-{
-    private String currentString = ""; 
-    private String[] startKeywords = {"", "[i]","[u]","[b]"};
-    private String[] endKeywords = {"","[/i]","[/u]","[/b]"};
-    private String[] keywordType = {"NORMAL","ITALIC", "UNDERLINE", "BOLD"};
-    private int currentIndex;
+	public BBParser(String current) {
+		this.currentIndex = 0;
+		this.current = current;
+	}
 
-    /**
-     * Creates a new instance of BBParser
-     */
-    public BBParser(String current)
-    {
-        currentIndex = 0;
-        currentString = current;
-    }
-    
-    /*
-     * Get the next token.
-     */    
-    public BBToken getNext()
-    {
-        String result = "";
-        int type = -1;
-        
-        while(hasNext())
-        {
-            if(currentString.charAt(currentIndex) == '[')
-            {
-                for (int i = 1; i < startKeywords.length; i++ )
-                {
-                    if(currentString.substring(currentIndex, currentIndex + startKeywords[i].length()).equalsIgnoreCase(startKeywords[i]))
-                    {
-                        if(type == 0)
-                            return new BBToken(result,keywordType[type]);
-                        else
-                        {
-                            type = i;
-                            currentIndex += startKeywords[type].length();
-                        }
-                    }
-                }
-                
-                if(type > 0)
-                {
-                    for (int i = 1; i < endKeywords.length; i++ )
-                    {
-                        if(currentString.substring(currentIndex, currentIndex + endKeywords[i].length()).equalsIgnoreCase(endKeywords[i]))
-                        {
-                            if(type == i)
-                            {
-                                currentIndex += endKeywords[i].length();
-                                return new BBToken(result,keywordType[type]);
-                            }
-                        }
-                    }
-                }
-            }
-            else if(type == -1 )
-                type = 0;
+	/*
+	 * Get the next token.
+	 */
+	public BBToken getNext() {
+		String result = new String();
+		int type = -1;
 
-            result += currentString.charAt(currentIndex);
-            currentIndex++;   
-        }
-        
-        return new BBToken(result,keywordType[type]);
-    }
-    
-    /*
-     *  See if there are more tokens.
-     */
-    public boolean hasNext()
-    {
-        return currentIndex < (currentString.length());
-    }
-    
-    
-    /*
-     * Example for testing
-     */
-    public static void main(String [] args)
-    {
-        BBParser p = new BBParser("Hello [b]there[/b], how are [i]you[/i]?");
-        while(p.hasNext())
-        {
-            BBToken t = p.getNext();
-            System.out.println(t.getValue() + " " + t.getType());
-        }
-        
-        p = new BBParser("[/i][/B]Hello [/i][B]there[/b][b][/b], how are [I]you[/i]?");
-        while(p.hasNext())
-        {
-            BBToken t = p.getNext();
-            System.out.println(t.getValue() + " " + t.getType());
-        }
-    }
+		while (hasNext()) {
+			if (current.charAt(currentIndex) == '[') {
+				for (int i = 1; i < startKeywords.length; i++) {
+					if (current.substring(currentIndex, currentIndex + startKeywords[i].length()).equalsIgnoreCase(startKeywords[i])) {
+						if (type == 0)
+							return new BBToken(result, keywordType[type]);
+						else {
+							type = i;
+							currentIndex += startKeywords[type].length();
+						}
+					}
+				}
+
+				if (type > 0) {
+					for (int i = 1; i < endKeywords.length; i++) {
+						if (current.substring(currentIndex, currentIndex + endKeywords[i].length()).equalsIgnoreCase(endKeywords[i])) {
+							if (type == i) {
+								currentIndex += endKeywords[i].length();
+								return new BBToken(result, keywordType[type]);
+							}
+						}
+					}
+				}
+			} else if (type == -1)
+				type = 0;
+
+			result += current.charAt(currentIndex);
+			currentIndex++;
+		}
+
+		return new BBToken(result, keywordType[type]);
+	}
+
+	/*
+	 * See if there are more tokens.
+	 */
+	public boolean hasNext() {
+		return currentIndex < (current.length());
+	}
+
+	/*
+	 * Example for testing
+	 */
+	public static void main(String[] args) {
+		BBParser p = new BBParser("Hello [b]there[/b], how are [i]you[/i]?");
+		while (p.hasNext()) {
+			BBToken t = p.getNext();
+			System.out.println(t.getValue() + " " + t.getType());
+		}
+
+		p = new BBParser("[/i][/B]Hello [/i][B]there[/b][b][/b], how are [I]you[/i]?");
+		while (p.hasNext()) {
+			BBToken t = p.getNext();
+			System.out.println(t.getValue() + " " + t.getType());
+		}
+	}
 }
--- server/src/org/mule/providers/pdf/BBToken.java
+++ server/src/org/mule/providers/pdf/BBToken.java
@@ -1,38 +1,19 @@
-/*
- * BBToken.java
- *
- * Created on October 4, 2006, 11:06 AM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
+package org.mule.providers.pdf;
 
-package com.webreach.mirth.client.ui.util;
+public class BBToken {
+	private String value;
+	private String type;
 
-/**
- *
- * @author brendanh
- */
-public class BBToken
-{
-    private String value;
-    private String type;
-    /**
-     * Creates a new instance of BBToken
-     */
-    public BBToken(String value, String type)
-    {
-        this.value = value;
-        this.type = type;
-    }
-    
-    public String getValue()
-    {
-        return value;
-    }
-    
-    public String getType()
-    {
-        return type;
-    }
+	public BBToken(String value, String type) {
+		this.value = value;
+		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public String getType() {
+		return type;
+	}
 }
