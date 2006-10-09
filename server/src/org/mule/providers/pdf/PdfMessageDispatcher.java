5c7fbd380e0e612a1c94fb5eab8817ba4750c944
Gerald Bortis <gbortis@nextgen.com>
2006-10-09 22:30:32 +0000

    Uses iText HTML parser rather than BBCode.

--- server/src/org/mule/providers/pdf/PdfMessageDispatcher.java
+++ server/src/org/mule/providers/pdf/PdfMessageDispatcher.java
@@ -1,11 +1,9 @@
 package org.mule.providers.pdf;
 
-import java.awt.Color;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
 import org.mule.providers.AbstractMessageDispatcher;
 import org.mule.providers.TemplateValueReplacer;
@@ -17,9 +15,7 @@ import org.mule.umo.endpoint.UMOEndpointURI;
 import org.mule.util.Utility;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.Font;
-import com.lowagie.text.Image;
-import com.lowagie.text.Phrase;
+import com.lowagie.text.html.HtmlParser;
 import com.lowagie.text.pdf.PdfWriter;
 import com.webreach.mirth.model.MessageObject;
 
@@ -74,59 +70,22 @@ public class PdfMessageDispatcher extends AbstractMessageDispatcher {
 		try {
 			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
 
-			if (connector.isEncrypted()) {
+			if (connector.isEncrypted() && (connector.getPassword() != null)) {
 				writer.setEncryption(PdfWriter.STRENGTH128BITS, connector.getPassword(), null, PdfWriter.AllowCopy | PdfWriter.AllowPrinting | PdfWriter.AllowFillIn);
 			}
 
+			// add tags to the template to create a valid HTML document
+			StringBuilder contents = new StringBuilder();
+			contents.append("<html>");
+			contents.append("<body>");
+			contents.append(template);
+			contents.append("</body>");
+			contents.append("</html>");
+
+			ByteArrayInputStream bais = new ByteArrayInputStream(contents.toString().getBytes());
 			document.open();
-			BBCodeParser parser = new BBCodeParser(template);
-
-			while (parser.hasNext()) {
-				BBCodeToken token = parser.getNext();
-				List<BBCodeParser.KeywordType> tags = token.getApplicableTags();
-
-				if (tags.contains(BBCodeParser.KeywordType.NEWPAGE)) {
-					document.newPage();
-				} else if (tags.contains(BBCodeParser.KeywordType.IMAGE)) {
-					Image image = Image.getInstance(token.getValue());
-					document.add(image);
-				} else if (tags.contains(BBCodeParser.KeywordType.PRE)) {
-					document.add(new Phrase(token.getValue(), new Font(Font.TIMES_ROMAN, Font.DEFAULTSIZE, Font.NORMAL)));
-				} else {
-					// set the style
-					int fontStyle = Font.NORMAL;
-
-					for (Iterator iter = tags.iterator(); iter.hasNext();) {
-						BBCodeParser.KeywordType keyword = (BBCodeParser.KeywordType) iter.next();
-
-						if (keyword.equals(BBCodeParser.KeywordType.BOLD)) {
-							fontStyle |= Font.BOLD;
-						} else if (keyword.equals(BBCodeParser.KeywordType.ITALIC)) {
-							fontStyle |= Font.ITALIC;
-						} else if (keyword.equals(BBCodeParser.KeywordType.UNDERLINE)) {
-							fontStyle |= Font.UNDERLINE;
-						} else if (keyword.equals(BBCodeParser.KeywordType.STRIKETHRU)) {
-							fontStyle |= Font.STRIKETHRU;
-						}
-					}
-
-					// set the size
-					int fontSize = Font.DEFAULTSIZE;
-
-					if (token.getSize() > 0) {
-						fontSize = token.getSize();
-					}
-
-					// set the color
-					Color fontColor = Color.BLACK;
-					
-					if (token.getColor() != null) {
-						fontColor = Color.getColor(token.getColor());
-					}
-					
-					document.add(new Phrase(token.getValue(), new Font(Font.TIMES_ROMAN, fontSize, fontStyle, fontColor)));
-				}
-			}
+			HtmlParser parser = new HtmlParser();
+			parser.go(document, bais);
 		} catch (Exception e) {
 			throw e;
 		} finally {
@@ -156,6 +115,6 @@ public class PdfMessageDispatcher extends AbstractMessageDispatcher {
 			return connector.getFilenameParser().getFilename(event.getMessage(), pattern);
 		}
 	}
-	
+
 	public void doDispose() {}
 }
