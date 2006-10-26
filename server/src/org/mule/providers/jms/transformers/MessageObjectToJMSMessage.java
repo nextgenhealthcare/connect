ad492672e7a99948182d8dd57c75e68de150560d
Gerald Bortis <gbortis@nextgen.com>
2006-10-26 22:45:13 +0000

    Message status is updated to SENT when transformed to a JMS message object.

--- server/src/org/mule/providers/jms/transformers/MessageObjectToJMSMessage.java
+++ server/src/org/mule/providers/jms/transformers/MessageObjectToJMSMessage.java
@@ -3,11 +3,19 @@ package org.mule.providers.jms.transformers;
 import org.mule.umo.transformer.TransformerException;
 
 import com.webreach.mirth.model.MessageObject;
+import com.webreach.mirth.server.controllers.MessageObjectController;
 
 public class MessageObjectToJMSMessage extends AbstractJmsTransformer {
+	private MessageObjectController messageObjectController = new MessageObjectController();
+	
 	public Object doTransform(Object src) throws TransformerException {
 		if (src instanceof MessageObject) {
 			MessageObject messageObject = (MessageObject) src;
+			
+			// update the message status to sent
+			messageObject.setStatus(MessageObject.Status.SENT);
+			messageObjectController.updateMessage(messageObject);
+
 			return transformToMessage(messageObject.getEncodedData());
 		}
 
