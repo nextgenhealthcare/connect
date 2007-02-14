625d07cabf6188dbd27ec8c014364bbcc0dd79e2
Gerald Bortis <gbortis@nextgen.com>
2007-02-14 20:48:20 +0000

    Removed unused import.

--- server/src/com/webreach/mirth/model/Transformer.java
+++ server/src/com/webreach/mirth/model/Transformer.java
@@ -29,7 +29,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.webreach.mirth.model.Connector.Mode;
 import com.webreach.mirth.model.MessageObject.Protocol;
 import com.webreach.mirth.util.EqualsUtil;
 
