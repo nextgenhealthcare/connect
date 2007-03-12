7defd8f8b50deef020a36782c2fa733101fb7d26
Gerald Bortis <gbortis@nextgen.com>
2007-03-12 19:20:17 +0000

    Fixed formatting.

--- server/src/com/webreach/mirth/server/util/ResponseFactory.java
+++ server/src/com/webreach/mirth/server/util/ResponseFactory.java
@@ -4,19 +4,23 @@ import com.webreach.mirth.model.Response;
 import com.webreach.mirth.model.Response.Status;
 
 public class ResponseFactory {
-	public Response getFailureResponse(String message){
+	public Response getFailureResponse(String message) {
 		return new Response(Status.FAILURE, message);
 	}
-	public Response getSuccessResponse(String message){
+
+	public Response getSuccessResponse(String message) {
 		return new Response(Status.SUCCESS, message);
 	}
-	public Response getFilteredResponse(String message){
+
+	public Response getFilteredResponse(String message) {
 		return new Response(Status.FILTERED, message);
 	}
-	public Response getQueudResponse(String message){
+
+	public Response getQueudResponse(String message) {
 		return new Response(Status.QUEUED, message);
 	}
-	public Response getResponse(String message){
+
+	public Response getResponse(String message) {
 		return new Response(Status.UNKNOWN, message);
 	}
 }
