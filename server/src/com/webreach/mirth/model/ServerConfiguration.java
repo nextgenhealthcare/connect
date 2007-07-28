79ebc22d278e3e71bff9320e3c25b1102816d89c
Chris Lang <clang@nextgen.com>
2007-07-28 01:47:09 +0000

    Added date field to ServerConfiguration

--- server/src/com/webreach/mirth/model/ServerConfiguration.java
+++ server/src/com/webreach/mirth/model/ServerConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Properties;
 
 public class ServerConfiguration implements Serializable {
+	private String date;
 	private List<Channel> channels;
 	private List<User> users;
 	private List<Alert> alerts;
@@ -40,5 +41,13 @@ public class ServerConfiguration implements Serializable {
 
 	public void setUsers(List<User> users) {
 		this.users = users;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
 	}
 }
