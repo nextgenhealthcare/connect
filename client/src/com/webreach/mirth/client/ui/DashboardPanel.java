5b2bc9ef183b4d95fd931f07a226d702c411cc16
Brendan Haverlock <bhaverlock@nextgen.com>
2007-08-02 01:13:10 +0000

    Formatted and renamed variables.

--- client/src/com/webreach/mirth/client/ui/DashboardPanel.java
+++ client/src/com/webreach/mirth/client/ui/DashboardPanel.java
@@ -39,7 +39,7 @@ import com.webreach.mirth.plugins.DashboardColumnPlugin;
 import com.webreach.mirth.plugins.TransformerStepPlugin;
 
 /**
- * 
+ *
  * @author brendanh
  */
 public class DashboardPanel extends javax.swing.JPanel
@@ -53,8 +53,8 @@ public class DashboardPanel extends javax.swing.JPanel
     private final String FILTERED_COLUMN_NAME = "Filtered";
     private int lastRow;
     private Frame parent;
-	private Map<String, DashboardColumnPlugin> loadedPlugins = new HashMap<String, DashboardColumnPlugin>();
-	
+    private Map<String, DashboardColumnPlugin> loadedColumnPlugins = new HashMap<String, DashboardColumnPlugin>();
+    
     /** Creates new form DashboardPanel */
     public DashboardPanel()
     {
@@ -70,12 +70,12 @@ public class DashboardPanel extends javax.swing.JPanel
             {
                 showStatusPopupMenu(evt, false);
             }
-
+            
             public void mouseReleased(java.awt.event.MouseEvent evt)
             {
                 showStatusPopupMenu(evt, false);
             }
-
+            
             public void mouseClicked(java.awt.event.MouseEvent evt)
             {
                 deselectRows();
@@ -84,48 +84,59 @@ public class DashboardPanel extends javax.swing.JPanel
         this.setDoubleBuffered(true);
     }
     // Extension point for ExtensionPoint.Type.CLIENT_VOCABULARY
-	@ExtensionPointDefinition(mode = ExtensionPoint.Mode.CLIENT, type = ExtensionPoint.Type.CLIENT_DASHBOARD_COLUMN)
-	public void loadPlugins() {
-		try {
-			Map<String, PluginMetaData> plugins = parent.mirthClient.getPluginMetaData();
-			for (PluginMetaData metaData : plugins.values()) {
-				if (metaData.isEnabled()) {
-					for (ExtensionPoint extensionPoint : metaData.getExtensionPoints()) {
-						try {
-							if (extensionPoint.getMode().equals(ExtensionPoint.Mode.CLIENT) && extensionPoint.getType().equals(ExtensionPoint.Type.CLIENT_DASHBOARD_COLUMN) && extensionPoint.getClassName() != null && extensionPoint.getClassName().length() > 0) {
-								String pluginName = extensionPoint.getName();
-								DashboardColumnPlugin columnPlugin = (DashboardColumnPlugin) Class.forName(extensionPoint.getClassName()).getDeclaredConstructors()[0].newInstance(new Object[]{pluginName, this});
-		                        loadedPlugins.put(columnPlugin.getColumnHeader(), columnPlugin);
-		                       }
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}
-
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    @ExtensionPointDefinition(mode = ExtensionPoint.Mode.CLIENT, type = ExtensionPoint.Type.CLIENT_DASHBOARD_COLUMN)
+    public void loadPlugins()
+    {
+        try
+        {
+            Map<String, PluginMetaData> plugins = parent.mirthClient.getPluginMetaData();
+            for (PluginMetaData metaData : plugins.values())
+            {
+                if (metaData.isEnabled())
+                {
+                    for (ExtensionPoint extensionPoint : metaData.getExtensionPoints())
+                    {
+                        try
+                        {
+                            if (extensionPoint.getMode().equals(ExtensionPoint.Mode.CLIENT) && extensionPoint.getType().equals(ExtensionPoint.Type.CLIENT_DASHBOARD_COLUMN) && extensionPoint.getClassName() != null && extensionPoint.getClassName().length() > 0)
+                            {
+                                String pluginName = extensionPoint.getName();
+                                DashboardColumnPlugin columnPlugin = (DashboardColumnPlugin) Class.forName(extensionPoint.getClassName()).getDeclaredConstructors()[0].newInstance(new Object[]{pluginName, this});
+                                loadedColumnPlugins.put(columnPlugin.getColumnHeader(), columnPlugin);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
     /**
      * Makes the status table with all current server information.
      */
     public void makeStatusTable()
     {
         updateTable();
-
+        
         statusTable.setDoubleBuffered(true);
-
+        
         statusTable.setSelectionMode(0);
-
+        
         statusTable.getColumnExt(STATUS_COLUMN_NAME).setMaxWidth(UIConstants.MAX_WIDTH);
         statusTable.getColumnExt(RECEIVED_COLUMN_NAME).setMaxWidth(UIConstants.MAX_WIDTH);
         statusTable.getColumnExt(SENT_COLUMN_NAME).setMaxWidth(UIConstants.MAX_WIDTH);
         statusTable.getColumnExt(ERROR_COLUMN_NAME).setMaxWidth(UIConstants.MAX_WIDTH);
         statusTable.getColumnExt(FILTERED_COLUMN_NAME).setMaxWidth(UIConstants.MAX_WIDTH);
         statusTable.getColumnExt(QUEUED_COLUMN_NAME).setMaxWidth(UIConstants.MAX_WIDTH);
-
+        
         statusTable.getColumnExt(STATUS_COLUMN_NAME).setMinWidth(UIConstants.MIN_WIDTH);
         statusTable.getColumnExt(RECEIVED_COLUMN_NAME).setMinWidth(UIConstants.MIN_WIDTH);
         statusTable.getColumnExt(SENT_COLUMN_NAME).setMinWidth(UIConstants.MIN_WIDTH);
@@ -133,38 +144,39 @@ public class DashboardPanel extends javax.swing.JPanel
         statusTable.getColumnExt(FILTERED_COLUMN_NAME).setMinWidth(UIConstants.MIN_WIDTH);
         statusTable.getColumnExt(QUEUED_COLUMN_NAME).setMinWidth(UIConstants.MIN_WIDTH);
         statusTable.getColumnExt(NAME_COLUMN_NAME).setMinWidth(UIConstants.MIN_WIDTH);
-
+        
         statusTable.getColumnExt(STATUS_COLUMN_NAME).setCellRenderer(new ImageCellRenderer());
         statusTable.getColumnExt(RECEIVED_COLUMN_NAME).setCellRenderer(new CenterCellRenderer());
         statusTable.getColumnExt(SENT_COLUMN_NAME).setCellRenderer(new CenterCellRenderer());
         statusTable.getColumnExt(ERROR_COLUMN_NAME).setCellRenderer(new CenterCellRenderer());
         statusTable.getColumnExt(FILTERED_COLUMN_NAME).setCellRenderer(new CenterCellRenderer());
         statusTable.getColumnExt(QUEUED_COLUMN_NAME).setCellRenderer(new CenterCellRenderer());
-
+        
         statusTable.getColumnExt(RECEIVED_COLUMN_NAME).setHeaderRenderer(PlatformUI.CENTER_COLUMN_HEADER_RENDERER);
         statusTable.getColumnExt(SENT_COLUMN_NAME).setHeaderRenderer(PlatformUI.CENTER_COLUMN_HEADER_RENDERER);
         statusTable.getColumnExt(ERROR_COLUMN_NAME).setHeaderRenderer(PlatformUI.CENTER_COLUMN_HEADER_RENDERER);
         statusTable.getColumnExt(FILTERED_COLUMN_NAME).setHeaderRenderer(PlatformUI.CENTER_COLUMN_HEADER_RENDERER);
         statusTable.getColumnExt(QUEUED_COLUMN_NAME).setHeaderRenderer(PlatformUI.CENTER_COLUMN_HEADER_RENDERER);
         
-        for(DashboardColumnPlugin plugin : loadedPlugins.values()){
-        	String columnName = plugin.getColumnHeader();
-        	statusTable.getColumnExt(columnName).setMaxWidth(UIConstants.MAX_WIDTH);
-        	statusTable.getColumnExt(columnName).setMinWidth(UIConstants.MIN_WIDTH);
-        	statusTable.getColumnExt(columnName).setCellRenderer(plugin.getCellRenderer());
-        	statusTable.getColumnExt(columnName).setHeaderRenderer(PlatformUI.CENTER_COLUMN_HEADER_RENDERER);
+        for(DashboardColumnPlugin plugin : loadedColumnPlugins.values())
+        {
+            String columnName = plugin.getColumnHeader();
+            statusTable.getColumnExt(columnName).setMaxWidth(UIConstants.MAX_WIDTH);
+            statusTable.getColumnExt(columnName).setMinWidth(UIConstants.MIN_WIDTH);
+            statusTable.getColumnExt(columnName).setCellRenderer(plugin.getCellRenderer());
+            statusTable.getColumnExt(columnName).setHeaderRenderer(PlatformUI.CENTER_COLUMN_HEADER_RENDERER);
         }
-        	
+        
         statusTable.packTable(UIConstants.COL_MARGIN);
-
+        
         statusTable.setRowHeight(UIConstants.ROW_HEIGHT);
         statusTable.setOpaque(true);
         statusTable.setRowSelectionAllowed(true);
-
+        
         statusTable.setSortable(true);
-
+        
         statusPane.setViewportView(statusTable);
-
+        
         statusTable.getSelectionModel().addListSelectionListener(new ListSelectionListener()
         {
             public void valueChanged(ListSelectionEvent evt)
@@ -178,12 +190,12 @@ public class DashboardPanel extends javax.swing.JPanel
             {
                 showStatusPopupMenu(evt, true);
             }
-
+            
             public void mouseReleased(java.awt.event.MouseEvent evt)
             {
                 showStatusPopupMenu(evt, true);
             }
-
+            
             public void mouseClicked(java.awt.event.MouseEvent evt)
             {
                 if (evt.getClickCount() >= 2)
@@ -191,14 +203,14 @@ public class DashboardPanel extends javax.swing.JPanel
             }
         });
     }
-
+    
     public synchronized void updateTable()
     {
         Object[][] tableData = null;
-
+        
         if (parent.status != null)
         {
-            tableData = new Object[parent.status.size()][7 + loadedPlugins.size()];
+            tableData = new Object[parent.status.size()][7 + loadedColumnPlugins.size()];
             for (int i = 0; i < parent.status.size(); i++)
             {
                 ChannelStatus tempStatus = parent.status.get(i);
@@ -211,29 +223,30 @@ public class DashboardPanel extends javax.swing.JPanel
                     tableData[i][5] = tempStats.getSent();
                     tableData[i][6] = tempStats.getError();
                     int j = 7;
-                    for (DashboardColumnPlugin plugin : loadedPlugins.values()){
-                    	tableData[i][j] = plugin.getTableData(tempStatus);
-                    	j++;
-                    } 
+                    for (DashboardColumnPlugin plugin : loadedColumnPlugins.values())
+                    {
+                        tableData[i][j] = plugin.getTableData(tempStatus);
+                        j++;
+                    }
                 }
                 catch (ClientException ex)
                 {
                     parent.alertException(ex.getStackTrace(), ex.getMessage());
                 }
-
+                
                 if (tempStatus.getState() == ChannelStatus.State.STARTED)
                     tableData[i][0] = new CellData(new ImageIcon(Frame.class.getResource("images/bullet_green.png")), "Started");
                 else if (tempStatus.getState() == ChannelStatus.State.STOPPED)
                     tableData[i][0] = new CellData(new ImageIcon(Frame.class.getResource("images/bullet_red.png")), "Stopped");
                 else if (tempStatus.getState() == ChannelStatus.State.PAUSED)
                     tableData[i][0] = new CellData(new ImageIcon(Frame.class.getResource("images/bullet_yellow.png")), "Paused");
-
+                
                 tableData[i][1] = tempStatus.getName();
-
+                
             }
-
+            
         }
-
+        
         if (statusTable != null)
         {
             lastRow = statusTable.getSelectedRow();
@@ -243,27 +256,29 @@ public class DashboardPanel extends javax.swing.JPanel
         else
         {
             statusTable = new MirthTable();
-            String[] defaultColumns = new String[] { STATUS_COLUMN_NAME, 
-            		NAME_COLUMN_NAME, RECEIVED_COLUMN_NAME, 
-            		FILTERED_COLUMN_NAME, QUEUED_COLUMN_NAME, 
-            		SENT_COLUMN_NAME, ERROR_COLUMN_NAME };
+            String[] defaultColumns = new String[] { STATUS_COLUMN_NAME,
+            NAME_COLUMN_NAME, RECEIVED_COLUMN_NAME,
+            FILTERED_COLUMN_NAME, QUEUED_COLUMN_NAME,
+            SENT_COLUMN_NAME, ERROR_COLUMN_NAME };
             ArrayList<String> columns = new ArrayList<String>();
-            for (int i = 0; i < defaultColumns.length; i++){
-            	columns.add(defaultColumns[i]);
+            for (int i = 0; i < defaultColumns.length; i++)
+            {
+                columns.add(defaultColumns[i]);
             }
-            for (DashboardColumnPlugin plugin : loadedPlugins.values()){
-            	columns.add(plugin.getColumnHeader());
-            } 
+            for (DashboardColumnPlugin plugin : loadedColumnPlugins.values())
+            {
+                columns.add(plugin.getColumnHeader());
+            }
             
             statusTable.setModel(new RefreshTableModel(tableData, columns.toArray(new String[0]))
             {
                 public boolean isCellEditable(int rowIndex, int columnIndex)
                 {
-                	return false;
+                    return false;
                 }
             });
         }
-
+        
         if (lastRow >= 0 && lastRow < statusTable.getRowCount())
             statusTable.setRowSelectionInterval(lastRow, lastRow);
         else
@@ -282,26 +297,26 @@ public class DashboardPanel extends javax.swing.JPanel
             @Override
             protected Color computeBackground(Component arg0, ComponentAdapter arg1)
             {
-                    arg0.setBackground(Color.PINK);
-                    return super.computeBackground(arg0, arg1);
+                arg0.setBackground(Color.PINK);
+                return super.computeBackground(arg0, arg1);
             }
-
+            
             @Override
             protected boolean test(ComponentAdapter adapter)
             {
-                    if (adapter.column == statusTable.getColumnNumber(ERROR_COLUMN_NAME))
-                    {
-                            if (((Integer)statusTable.getValueAt(adapter.row, adapter.column)).intValue() > 0)
-                                    return true;
-                    }
-                    return false;
+                if (adapter.column == statusTable.getColumnNumber(ERROR_COLUMN_NAME))
+                {
+                    if (((Integer)statusTable.getValueAt(adapter.row, adapter.column)).intValue() > 0)
+                        return true;
+                }
+                return false;
             }
         };
-
+        
         highlighter.addHighlighter(errorHighlighter);
         statusTable.setHighlighters(highlighter);
     }
-
+    
     /**
      * Shows the popup menu when the trigger button (right-click) has been
      * pushed.
@@ -320,7 +335,7 @@ public class DashboardPanel extends javax.swing.JPanel
             parent.statusPopupMenu.show(evt.getComponent(), evt.getX(), evt.getY());
         }
     }
-
+    
     /*
      * Action when something on the status list has been selected. Sets all
      * appropriate tasks visible.
@@ -355,7 +370,7 @@ public class DashboardPanel extends javax.swing.JPanel
             }
         }
     }
-
+    
     /**
      * Gets the index of the selected status row.
      */
@@ -368,13 +383,13 @@ public class DashboardPanel extends javax.swing.JPanel
         }
         return UIConstants.ERROR_CONSTANT;
     }
-
+    
     public void deselectRows()
     {
         statusTable.deselectRows();
         parent.setVisibleTasks(parent.statusTasks, parent.statusPopupMenu, 2, -1, false);
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -395,11 +410,11 @@ public class DashboardPanel extends javax.swing.JPanel
         layout.setHorizontalGroup(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING).add(statusPane, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE));
         layout.setVerticalGroup(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING).add(statusPane, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE));
     }// </editor-fold>//GEN-END:initComponents
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane statusPane;
 
     private com.webreach.mirth.client.ui.components.MirthTable statusTable;
     // End of variables declaration//GEN-END:variables
-
+    
 }
