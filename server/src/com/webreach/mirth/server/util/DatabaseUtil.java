5197e1ef4eaeb478ff95bb474e0423508dde47da
Brendan Haverlock <bhaverlock@nextgen.com>
2007-08-04 01:06:30 +0000

    Added .commit() to commit the migration.

--- server/src/com/webreach/mirth/server/util/DatabaseUtil.java
+++ server/src/com/webreach/mirth/server/util/DatabaseUtil.java
@@ -125,7 +125,8 @@ public class DatabaseUtil {
             }
             
             statement.executeBatch();
-                  
+            
+            conn.commit();                 
         } catch (Exception e) {
             throw new Exception(e);
         } finally {
