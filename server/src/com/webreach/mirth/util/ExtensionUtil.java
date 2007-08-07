493b97b387797ce0ee873a067a54db7fff1c5eef
Gerald Bortis <gbortis@nextgen.com>
2007-08-07 22:33:27 +0000

    Ran server formatter.

--- server/src/com/webreach/mirth/util/ExtensionUtil.java
+++ server/src/com/webreach/mirth/util/ExtensionUtil.java
@@ -25,151 +25,121 @@ import com.webreach.mirth.server.controllers.ControllerException;
 import com.webreach.mirth.server.util.FileUtil;
 import com.webreach.mirth.server.util.UUIDGenerator;
 
-public class ExtensionUtil
-{
-    public static Map<String, ? extends MetaData> loadExtensionMetaData(String location) throws ControllerException
-    {
-        FileFilter fileFilter = new FileFilter()
-        {
-            public boolean accept(File file)
-            {
-                return (!file.isDirectory() && file.getName().endsWith(".xml"));
-            }
-        };
+public class ExtensionUtil {
+	public static Map<String, ? extends MetaData> loadExtensionMetaData(String location) throws ControllerException {
+		FileFilter fileFilter = new FileFilter() {
+			public boolean accept(File file) {
+				return (!file.isDirectory() && file.getName().endsWith(".xml"));
+			}
+		};
 
-        Map<String, MetaData> extensionMap = new HashMap<String, MetaData>();
-        File path = new File(location);
-        File[] extensionFiles = path.listFiles(fileFilter);
-        ObjectXMLSerializer serializer = new ObjectXMLSerializer(new Class[]{PluginMetaData.class, ConnectorMetaData.class});
+		Map<String, MetaData> extensionMap = new HashMap<String, MetaData>();
+		File path = new File(location);
+		File[] extensionFiles = path.listFiles(fileFilter);
+		ObjectXMLSerializer serializer = new ObjectXMLSerializer(new Class[] { PluginMetaData.class, ConnectorMetaData.class });
 
-        try
-        {
-            for (int i = 0; i < extensionFiles.length; i++)
-            {
-                File extensionFile = extensionFiles[i];
-                String xml = FileUtil.read(extensionFile.getAbsolutePath());
-                MetaData extensionMetadata = (MetaData) serializer.fromXML(xml);
-                extensionMap.put(extensionMetadata.getName(), extensionMetadata);
-            }
-        }
-        catch (IOException ioe)
-        {
-            throw new ControllerException(ioe);
-        }
+		try {
+			for (int i = 0; i < extensionFiles.length; i++) {
+				File extensionFile = extensionFiles[i];
+				String xml = FileUtil.read(extensionFile.getAbsolutePath());
+				MetaData extensionMetadata = (MetaData) serializer.fromXML(xml);
+				extensionMap.put(extensionMetadata.getName(), extensionMetadata);
+			}
+		} catch (IOException ioe) {
+			throw new ControllerException(ioe);
+		}
 
-        return extensionMap;
-    }
+		return extensionMap;
+	}
 
-    public static void saveExtensionMetaData(Map<String, ? extends MetaData> metaData, String location) throws ControllerException
-    {
-        ObjectXMLSerializer serializer = new ObjectXMLSerializer(new Class[]{PluginMetaData.class, ConnectorMetaData.class});
+	public static void saveExtensionMetaData(Map<String, ? extends MetaData> metaData, String location) throws ControllerException {
+		ObjectXMLSerializer serializer = new ObjectXMLSerializer(new Class[] { PluginMetaData.class, ConnectorMetaData.class });
 
-        try
-        {
-            Iterator i = metaData.entrySet().iterator();
-            while (i.hasNext())
-            {
-                Entry entry = (Entry) i.next();
-                FileUtil.write(location + entry.getKey().toString() + ".xml", false, serializer.toXML(metaData.get(entry.getKey())));
+		try {
+			Iterator i = metaData.entrySet().iterator();
+			while (i.hasNext()) {
+				Entry entry = (Entry) i.next();
+				FileUtil.write(location + entry.getKey().toString() + ".xml", false, serializer.toXML(metaData.get(entry.getKey())));
 
-            }
-        }
-        catch (IOException ioe)
-        {
-            throw new ControllerException(ioe);
-        }
+			}
+		} catch (IOException ioe) {
+			throw new ControllerException(ioe);
+		}
 
-    }
+	}
 
-    public static List<String> loadExtensionLibraries(String location) throws ControllerException
-    {
-        // update this to use regular expression to get the client and shared
-        // libraries
-        FileFilter libraryFilter = new FileFilter()
-        {
-            public boolean accept(File file)
-            {
-                return (!file.isDirectory() && (file.getName().contains("-client.jar") || file.getName().contains("-shared.jar")));
-            }
-        };
+	public static List<String> loadExtensionLibraries(String location) throws ControllerException {
+		// update this to use regular expression to get the client and shared
+		// libraries
+		FileFilter libraryFilter = new FileFilter() {
+			public boolean accept(File file) {
+				return (!file.isDirectory() && (file.getName().contains("-client.jar") || file.getName().contains("-shared.jar")));
+			}
+		};
 
-        List<String> extensionLibs = new ArrayList<String>();
-        File path = new File(location);
-        File[] extensionFiles = path.listFiles(libraryFilter);
+		List<String> extensionLibs = new ArrayList<String>();
+		File path = new File(location);
+		File[] extensionFiles = path.listFiles(libraryFilter);
 
-        for (int i = 0; i < extensionFiles.length; i++)
-        {
-            File extensionFile = extensionFiles[i];
-            extensionLibs.add(extensionFile.getName());
-        }
+		for (int i = 0; i < extensionFiles.length; i++) {
+			File extensionFile = extensionFiles[i];
+			extensionLibs.add(extensionFile.getName());
+		}
 
-        return extensionLibs;
-    }
+		return extensionLibs;
+	}
 
-    public static void installExtension(String location, byte[] contents) throws ControllerException
-    {
-        // update this to use regular expression to get the client and shared
-        // libraries
-        String uniqueId = UUIDGenerator.getUUID();
+	public static void installExtension(String location, byte[] contents) throws ControllerException {
+		// update this to use regular expression to get the client and shared
+		// libraries
+		String uniqueId = UUIDGenerator.getUUID();
 
-        ZipFile zipFile = null;
-        try
-        {
-            File file = File.createTempFile(uniqueId, ".zip");
-            String zipFileLocation = file.getAbsolutePath();       
-           
-            FileUtil.write(zipFileLocation, false, contents);
-            zipFile = new ZipFile(zipFileLocation);
+		ZipFile zipFile = null;
+		try {
+			File file = File.createTempFile(uniqueId, ".zip");
+			String zipFileLocation = file.getAbsolutePath();
 
-            Enumeration entries = zipFile.entries();
+			FileUtil.write(zipFileLocation, false, contents);
+			zipFile = new ZipFile(zipFileLocation);
 
-            while (entries.hasMoreElements())
-            {
-                ZipEntry entry = (ZipEntry) entries.nextElement();
+			Enumeration entries = zipFile.entries();
 
-                if (entry.isDirectory())
-                {
-                    // Assume directories are stored parents first then
-                    // children.
+			while (entries.hasMoreElements()) {
+				ZipEntry entry = (ZipEntry) entries.nextElement();
 
-                    // This is not robust, just for demonstration purposes.
-                    (new File(entry.getName())).mkdir();
-                    continue;
-                }
+				if (entry.isDirectory()) {
+					// Assume directories are stored parents first then
+					// children.
 
-                copyInputStream(zipFile.getInputStream(entry), new BufferedOutputStream(new FileOutputStream(new File(location + entry.getName()))));
-            }
-        }
-        catch (Exception e)
-        {
-            throw new ControllerException(e);
-        }
-        finally
-        {
-            if (zipFile != null)
-            {
-                try
-                {
-                    zipFile.close();
-                }
-                catch (Exception e)
-                {
-                    throw new ControllerException(e);
-                }
-            }
-        }
-    }
+					// This is not robust, just for demonstration purposes.
+					(new File(entry.getName())).mkdir();
+					continue;
+				}
 
-    public static final void copyInputStream(InputStream in, OutputStream out) throws IOException
-    {
-        byte[] buffer = new byte[1024];
-        int len;
+				copyInputStream(zipFile.getInputStream(entry), new BufferedOutputStream(new FileOutputStream(new File(location + entry.getName()))));
+			}
+		} catch (Exception e) {
+			throw new ControllerException(e);
+		} finally {
+			if (zipFile != null) {
+				try {
+					zipFile.close();
+				} catch (Exception e) {
+					throw new ControllerException(e);
+				}
+			}
+		}
+	}
 
-        while ((len = in.read(buffer)) >= 0)
-            out.write(buffer, 0, len);
+	public static final void copyInputStream(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[1024];
+		int len;
 
-        in.close();
-        out.close();
-    }
+		while ((len = in.read(buffer)) >= 0)
+			out.write(buffer, 0, len);
+
+		in.close();
+		out.close();
+	}
 
 }
