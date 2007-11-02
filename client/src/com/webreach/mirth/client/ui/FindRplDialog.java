eaeef38166bfe90baefa26e4784215ee3c123ef6
Dan Svanstedt <dsvanstedt@nextgen.com>
2007-11-02 21:02:49 +0000

    only do "Case Sensitive" when we are not doing regular expression.

--- client/src/com/webreach/mirth/client/ui/FindRplDialog.java
+++ client/src/com/webreach/mirth/client/ui/FindRplDialog.java
@@ -56,10 +56,6 @@ public class FindRplDialog extends javax.swing.JDialog {
     void find(){
         String text = search_text.getText();
         String search = mirthTextField1.getText();
-        if(!mirthCheckBox2.isSelected()){
-            text = text.toUpperCase();
-            search = search.toUpperCase();
-        }
         if(mirthCheckBox1.isSelected()){
             // do regular expression
             Pattern p = Pattern.compile(search);
@@ -73,6 +69,11 @@ public class FindRplDialog extends javax.swing.JDialog {
             }
         }
         else {
+            // check for case sensitive
+            if(!mirthCheckBox2.isSelected()){
+                text = text.toUpperCase();
+                search = search.toUpperCase();
+            }
             int position = text.indexOf(search,search_text.getSelectionEnd());
             if(position > -1){
                 search_text.select(position,position+search.length());
