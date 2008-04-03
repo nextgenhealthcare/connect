40d0e585decf9308ee9a13b1eb75a3bbbe8d8f39
Jacob Brauer <jbrauer@nextgen.com>
2008-04-03 01:38:15 +0000

    Rewrote ACKGenerator to generate acks without serializing to XML. Issue: MIRTH-818

--- server/src/com/webreach/mirth/connectors/mllp/ACKGenerator.java
+++ server/src/com/webreach/mirth/connectors/mllp/ACKGenerator.java
@@ -1,49 +1,82 @@
 package com.webreach.mirth.connectors.mllp;
 
+import java.util.regex.Pattern;
+
 import org.apache.log4j.Logger;
 
-import com.webreach.mirth.model.converters.SerializerFactory;
 import com.webreach.mirth.server.util.DateUtil;
+import com.webreach.mirth.util.StringUtil;
+
 //Supports ACKS from 2.1-2.4
 //2.5 is supported but the advanced fields in ERR and SFT are not supported
 public class ACKGenerator {
 	private final String DEFAULTDATEFORMAT = "yyyyMMddHHmmss";
 	private Logger logger = Logger.getLogger(this.getClass());
+
 	public String generateAckResponse(String message, String acknowledgementCode, String textMessage) throws Exception {
 		return generateAckResponse(message, acknowledgementCode, textMessage, DEFAULTDATEFORMAT, new String());
 	}
-	
-	public String generateAckResponse(String message, String acknowledgementCode, String textMessage, String dateFormat, String errorMessage) throws Exception{
-		if (message == null || message.length() < 3) {
+
+	public String generateAckResponse(String message, String acknowledgementCode, String textMessage, String dateFormat, String errorMessage) throws Exception {
+		if (message == null || message.length() < 9) {
 			logger.error("Unable to parse, message is null or too short: " + message);
 			throw new Exception("Unable to parse, message is null or too short: " + message);
 		}
-		String segmentDelim = "\r";
+		message = StringUtil.convertLFtoCR(message);
+		char segmentDelim = '\r';
 		char fieldDelim = message.charAt(3); // Usually |
 		char elementDelim = message.charAt(4); // Usually ^
 		StringBuilder ackBuilder = new StringBuilder();
 		ackBuilder.append(message.substring(0, 9));
-		String originalXML;
-		originalXML = SerializerFactory.getHL7Serializer(false, false).toXML(message);			
-	    String timestamp = DateUtil.getCurrentDate(dateFormat);
-	    if (textMessage != null && textMessage.length() > 0){
-	    	textMessage = "|" + textMessage;
-	    }else{
-	    	textMessage = new String();
-	    }
-	    if (errorMessage != null && errorMessage.length() > 0){
-	    	errorMessage = segmentDelim + "ERR|" + errorMessage;
-	    }else{
-	    	errorMessage = new String();
-	    }
-	    String sendingApplication = getXMLValue(originalXML, "<MSH.3.1>", "</MSH.3.1>");
-	    String sendingFacility = getXMLValue(originalXML, "<MSH.4.1>", "</MSH.4.1>");
-	    String receivingApplication = getXMLValue(originalXML, "<MSH.5.1>", "</MSH.5.1>");
-	    String receivingFacility = getXMLValue(originalXML, "<MSH.6.1>", "</MSH.6.1>");
-	    String originalid = getXMLValue(originalXML, "<MSH.10.1>", "</MSH.10.1>");
-		String version = getXMLValue(originalXML, "<MSH.12.1>", "</MSH.12.1>");
-		String procid = getXMLValue(originalXML, "<MSH.11.1>", "</MSH.11.1>");
-		String procidmode = getXMLValue(originalXML, "<MSH.11.2>", "</MSH.11.2>");
+		String timestamp = DateUtil.getCurrentDate(dateFormat);
+		if (textMessage != null && textMessage.length() > 0) {
+			textMessage = fieldDelim + textMessage;
+		} else {
+			textMessage = new String();
+		}
+		if (errorMessage != null && errorMessage.length() > 0) {
+			errorMessage = segmentDelim + "ERR" + fieldDelim + errorMessage;
+		} else {
+			errorMessage = new String();
+		}
+
+		String mshString = message.substring(0, message.indexOf(String.valueOf(segmentDelim)));
+
+		Pattern fieldPattern = Pattern.compile(Pattern.quote(String.valueOf(fieldDelim)));
+		Pattern elementPattern = Pattern.compile(Pattern.quote(String.valueOf(elementDelim)));
+
+		String[] mshFields = fieldPattern.split(mshString);
+		int mshFieldsLength = mshFields.length;
+
+		String sendingApplication = ""; // MSH.3.1
+		String sendingFacility = ""; // MSH.4.1
+		String receivingApplication = ""; // MSH.5.1
+		String receivingFacility = ""; // MSH.6.1
+		String originalid = ""; // MSH.10.1
+		String procid = ""; // MSH.11.1
+		String procidmode = ""; // // MSH.11.2
+		String version = ""; // MSH.12.1
+
+		if (mshFieldsLength > 10) {
+			version = elementPattern.split(mshFields[11])[0]; // MSH.12.1
+		} else if (mshFieldsLength > 9) {
+			String[] msh11 = elementPattern.split(mshFields[10]); // MSH.11
+			procid = msh11[0]; // MSH.11.1
+			if (msh11.length > 1) {
+				procidmode = msh11[1]; // MSH.11.2
+			}
+		} else if (mshFieldsLength > 8) {
+			originalid = elementPattern.split(mshFields[9])[0]; // MSH.10.1
+		} else if (mshFieldsLength > 4) {
+			receivingFacility = elementPattern.split(mshFields[5])[0]; // MSH.6.1
+		} else if (mshFieldsLength > 3) {
+			receivingApplication = elementPattern.split(mshFields[4])[0]; // MSH.5.1
+		} else if (mshFieldsLength > 2) {
+			sendingFacility = elementPattern.split(mshFields[3])[0]; // MSH.4.1
+		} else if (mshFieldsLength > 1) {
+			sendingApplication = elementPattern.split(mshFields[2])[0]; // MSH.3.1
+		}
+
 		ackBuilder.append(receivingApplication);
 		ackBuilder.append(fieldDelim);
 		ackBuilder.append(receivingFacility);
@@ -60,8 +93,7 @@ public class ACKGenerator {
 		ackBuilder.append(timestamp);
 		ackBuilder.append(fieldDelim);
 		ackBuilder.append(procid);
-		if (procidmode != null && procidmode.length() > 0 )
-		{		
+		if (procidmode != null && procidmode.length() > 0) {
 			ackBuilder.append(elementDelim);
 			ackBuilder.append(procidmode);
 		}
@@ -75,16 +107,8 @@ public class ACKGenerator {
 		ackBuilder.append(originalid);
 		ackBuilder.append(textMessage);
 		ackBuilder.append(errorMessage);
-		ackBuilder.append(segmentDelim); //MIRTH-494
-		//MSH|^~\\&|{sendapp}|{sendfac}|{recapp}|{recfac}|{timestamp}||ACK|{timestamp}|P|{version}\rMSA|{code}|{originalid}{textmessage}\r
+		ackBuilder.append(segmentDelim); // MIRTH-494
+		// MSH|^~\\&|{sendapp}|{sendfac}|{recapp}|{recfac}|{timestamp}||ACK|{timestamp}|P|{version}\rMSA|{code}|{originalid}{textmessage}\r
 		return ackBuilder.toString();
-	}
-	private String getXMLValue(String source, String startTag, String endTag) {
-		String returnValue = "";
-		int startLoc = -1;
-		if ((startLoc = source.indexOf(startTag)) != -1) {
-			returnValue = source.substring(startLoc + startTag.length(), source.indexOf(endTag, startLoc));
-		}
-		return returnValue;
 	}
 }
