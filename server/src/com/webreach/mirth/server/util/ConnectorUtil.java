738c59fc1939808b73d2888d7f4da57b9c20a459
Gerald Bortis <gbortis@nextgen.com>
2008-12-18 23:40:07 +0000

    Added port to testConnection response message. Issue: MIRTH-1012

--- server/src/com/webreach/mirth/server/util/ConnectorUtil.java
+++ server/src/com/webreach/mirth/server/util/ConnectorUtil.java
@@ -18,11 +18,11 @@ public class ConnectorUtil {
         try {
             socket = new Socket();
             socket.connect(address, timeout);
-            return "Sucessfully connected to host: " + address.getAddress().getHostAddress();
+            return "Sucessfully connected to host: " + address.getAddress().getHostAddress() + ":" + address.getPort();
         } catch (SocketTimeoutException ste) {
-            return "Timed out connecting to host: " + address.getAddress().getHostAddress();
+            return "Timed out connecting to host: " + address.getAddress().getHostAddress() + ":" + address.getPort();
         } catch (Exception e) {
-            return "Could not connect to host: " + address.getAddress().getHostAddress();
+            return "Could not connect to host: " + address.getAddress().getHostAddress() + ":" + address.getPort();
         } finally {
             if (socket != null) {
                 socket.close();
