51f511625cb44940a9888daf58647dacb9772fcc
Gerald Bortis <gbortis@nextgen.com>
2008-12-18 22:53:07 +0000

    * Added ConnectorUtil with testConnection method * Added connector service to TCP and HTTP Issue: MIRTH-1012

--- server/src/com/webreach/mirth/connectors/tcp/TcpConnectorService.java
+++ server/src/com/webreach/mirth/connectors/tcp/TcpConnectorService.java
@@ -1,17 +1,17 @@
-package com.webreach.mirth.connectors.mllp;
+package com.webreach.mirth.connectors.tcp;
 
 import java.util.Map;
 
 import com.webreach.mirth.connectors.ConnectorService;
 import com.webreach.mirth.server.util.ConnectorUtil;
 
-public class MllpConnectorService implements ConnectorService {
+public class TcpConnectorService implements ConnectorService {
     public Object invoke(String method, Object object, String sessionsId) throws Exception {
         if (method.equals("testConnection")) {
             Map<String, String> params = (Map<String, String>) object;
-            String host = params.get(LLPSenderProperties.LLP_ADDRESS);
-            int port = Integer.parseInt(params.get(LLPSenderProperties.LLP_PORT));
-            int timeout = Integer.parseInt(params.get(LLPSenderProperties.LLP_SERVER_TIMEOUT));
+            String host = params.get(TCPSenderProperties.TCP_ADDRESS);
+            int port = Integer.parseInt(params.get(TCPSenderProperties.TCP_PORT));
+            int timeout = Integer.parseInt(params.get(TCPSenderProperties.TCP_SERVER_TIMEOUT));
             return ConnectorUtil.testConnection(host, port, timeout);
         }
 
---
+++ server/src/com/webreach/mirth/server/util/ConnectorUtil.java
@@ -0,0 +1,32 @@
+package com.webreach.mirth.server.util;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class ConnectorUtil {
+    public static String testConnection(String host, int port, int timeout) throws Exception {
+        Socket socket = null;
+        InetSocketAddress address = null;
+
+        try {
+            address = new InetSocketAddress(host, port);
+        } catch (Exception e) {
+            return "Invalid host or port.";
+        }
+
+        try {
+            socket = new Socket();
+            socket.connect(address, timeout);
+            return "Sucessfully connected to host: " + address.getAddress().getHostAddress();
+        } catch (SocketTimeoutException ste) {
+            return "Timed out connecting to host: " + address.getAddress().getHostAddress();
+        } catch (Exception e) {
+            return "Could not connect to host: " + address.getAddress().getHostAddress();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
