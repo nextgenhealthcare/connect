0fd0b372d1e4d69f1c841b3110f35e1a99b367e9
Jacob Brauer <jbrauer@nextgen.com>
2009-08-03 19:12:39 +0000

    Added use authentication option to the email sender. Issue: MIRTH-1101

--- server/src/com/webreach/mirth/connectors/email/EmailSenderProperties.java
+++ server/src/com/webreach/mirth/connectors/email/EmailSenderProperties.java
@@ -39,6 +39,7 @@ public class EmailSenderProperties implements ComponentProperties
     public static final String EMAIL_ADDRESS = "hostname";
     public static final String EMAIL_PORT = "smtpPort";
     public static final String EMAIL_SECURE = "emailSecure";
+    public static final String EMAIL_USE_AUTHENTICATION = "useAuthentication";
     public static final String EMAIL_USERNAME = "username";
     public static final String EMAIL_PASSWORD = "password";
     public static final String EMAIL_TO = "toAddresses";
@@ -58,6 +59,7 @@ public class EmailSenderProperties implements ComponentProperties
         properties.put(EMAIL_ADDRESS, "");
         properties.put(EMAIL_PORT, "");
         properties.put(EMAIL_SECURE, "none");
+        properties.put(EMAIL_USE_AUTHENTICATION, "1");
         properties.put(EMAIL_USERNAME, "");
         properties.put(EMAIL_PASSWORD, "");
         properties.put(EMAIL_TO, "");
