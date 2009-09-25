ad91f8b2e7b0cb43da5f6b414c6fe51e6778d759
Dan Svanstedt <dsvanstedt@nextgen.com>
2009-09-25 22:22:10 +0000

    MIRTH-1178 and MIRTH-1057: Now we can pass in sliceindex to get specific image in imagestack. Also fixes some issues with jpeg encoding.

--- server/src/com/webreach/mirth/server/util/DICOMUtil.java
+++ server/src/com/webreach/mirth/server/util/DICOMUtil.java
@@ -1,16 +1,17 @@
 package com.webreach.mirth.server.util;
 
 import ij.plugin.DICOM;
+import ij.ImagePlus;
+import ij.ImageStack;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
-import javax.imageio.ImageIO;
+import javax.imageio.*;
 
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
@@ -104,7 +105,10 @@ public class DICOMUtil {
     {
         String encodedData = getDICOMRawData(message);
         BASE64Decoder decoder = new BASE64Decoder();
-        BASE64Encoder base64Encoder = new BASE64Encoder();           
+        BASE64Encoder base64Encoder = new BASE64Encoder();
+        // use new method for jpegs
+        if(format.equalsIgnoreCase("jpg") || format.equalsIgnoreCase("jpeg"))
+            return base64Encoder.encode(dicomToJpg(1,message));
         try {
             byte[] rawImage = decoder.decodeBuffer(encodedData);
             ByteArrayInputStream bis = new ByteArrayInputStream(rawImage);
@@ -129,5 +133,52 @@ public class DICOMUtil {
     public static String reAttachMessage(MessageObject message){
         return AttachmentUtil.reAttachMessage(message);
     }
-    
+
+        public static byte[] dicomToJpg(int sliceIndex, MessageObject message){
+            String encodedData = getDICOMRawData(message);
+            BASE64Decoder decoder = new BASE64Decoder();
+            try {
+                ByteArrayInputStream bis = new ByteArrayInputStream(decoder.decodeBuffer(encodedData));
+                DICOM dcm = new DICOM(bis);
+                dcm.run("dcm");
+                ImageStack imageStack = dcm.getImageStack();
+                if(imageStack.getSize() < sliceIndex || sliceIndex < 1) { return null; }
+                ImagePlus image = new ImagePlus("ImageName", imageStack.getProcessor(sliceIndex));
+                return saveAsJpeg(image, 100);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        static byte[] saveAsJpeg(ImagePlus imp, int quality) {
+        int width = imp.getWidth();
+        int height = imp.getHeight();
+        int biType = BufferedImage.TYPE_INT_RGB;
+        if (imp.getProcessor().isDefaultLut())
+            biType = BufferedImage.TYPE_BYTE_GRAY;
+        BufferedImage bi = new BufferedImage(width, height, biType);
+        String error = null;
+        try {
+            Graphics g = bi.createGraphics();
+            g.drawImage(imp.getImage(), 0, 0, null);
+            g.dispose();
+            Iterator iter = ImageIO.getImageWritersByFormatName("jpg");
+            ImageWriter writer = (ImageWriter)iter.next();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            writer.setOutput(ImageIO.createImageOutputStream(baos));
+            ImageWriteParam param = writer.getDefaultWriteParam();
+            param.setCompressionMode(param.MODE_EXPLICIT);
+            param.setCompressionQuality(quality/100f);
+            if (quality == 100)
+                param.setSourceSubsampling(1, 1, 0, 0);
+            IIOImage iioImage = new IIOImage(bi, null, null);
+            writer.write(null, iioImage, param);
+            return baos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
