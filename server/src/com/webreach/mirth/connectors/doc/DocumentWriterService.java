148df99bb9286d163e64ac542e721fdefb718997
Gerald Bortis <gbortis@nextgen.com>
2009-10-09 21:01:25 +0000

    Added test write on Document Writer Issue: MIRTH-1218

---
+++ server/src/com/webreach/mirth/connectors/doc/DocumentWriterService.java
@@ -0,0 +1,33 @@
+package com.webreach.mirth.connectors.doc;
+
+import java.util.Map;
+
+import com.webreach.mirth.connectors.ConnectorService;
+import com.webreach.mirth.connectors.file.FileConnector;
+import com.webreach.mirth.connectors.file.filesystems.FileSystemConnection;
+import com.webreach.mirth.connectors.file.filesystems.FileSystemConnectionFactory;
+import com.webreach.mirth.util.ConnectionTestResponse;
+
+public class DocumentWriterService implements ConnectorService {
+    public Object invoke(String method, Object object, String sessionsId) throws Exception {
+        if (method.equals("testWrite")) {
+            Map<String, String> params = (Map<String, String>) object;
+            String directory = params.get(DocumentWriterProperties.FILE_DIRECTORY);
+
+            FileSystemConnectionFactory factory = new FileSystemConnectionFactory(FileConnector.SCHEME_FILE, null, null, directory, 0, false, false);
+            FileSystemConnection connection = (FileSystemConnection) factory.makeObject();
+
+            try {
+                if (connection.canWrite(directory)) {
+                    return new ConnectionTestResponse(ConnectionTestResponse.Type.SUCCESS, "Sucessfully connected to: " + directory);
+                } else {
+                    return new ConnectionTestResponse(ConnectionTestResponse.Type.FAILURE, "Unable to connect to: " + directory);
+                }
+            } catch (Exception e) {
+                return new ConnectionTestResponse(ConnectionTestResponse.Type.FAILURE, "Unable to connect to: " + directory + ", Reason: " + e.getMessage());
+            }
+        }
+
+        return null;
+    }
+}
