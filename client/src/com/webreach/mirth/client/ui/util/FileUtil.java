827f1bcbf7d29532f76b6cebabb16c66412741fd
Jacob Brauer <jbrauer@nextgen.com>
2009-10-13 01:02:24 +0000

    Changed the client FileUtil to use UTF-8 for cross platform export/import with special characters Issue: MIRTH-1222

--- client/src/com/webreach/mirth/client/ui/util/FileUtil.java
+++ client/src/com/webreach/mirth/client/ui/util/FileUtil.java
@@ -25,81 +25,79 @@
 
 package com.webreach.mirth.client.ui.util;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 import org.apache.commons.codec.binary.Base64;
 
-public class FileUtil
-{
-    public static void write(File file, String data, boolean append) throws IOException
-    {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file, append));
+public class FileUtil {
 
-        try
-        {
+    private static final String CHARSET = "UTF-8";
+
+    public static void write(File file, String data, boolean append) throws IOException {
+        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file, append), CHARSET);
+
+        try {
             writer.write(data);
             writer.flush();
-        }
-        finally
-        {
+        } finally {
             writer.close();
         }
     }
 
-    public static String read(File file) throws IOException
-    {        
-        StringBuilder fileData = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        char[] buf = new char[1024];
-        int numRead=0;
-        while((numRead=reader.read(buf)) != -1){
-            fileData.append(buf, 0, numRead);
+    public static String read(File file) throws IOException {
+        FileInputStream stream = new FileInputStream(file);
+        try {
+            // This is the quickest way to read a file
+            FileChannel fileChannel = stream.getChannel();
+            MappedByteBuffer byteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            // Decode the bytes to a string using the given charset
+            return Charset.forName(CHARSET).decode(byteBuffer).toString();
+        } finally {
+            stream.close();
         }
-        reader.close();
-        return fileData.toString();
     }
-    
-    public static String readBinaryBase64(File file) throws IOException
-    {        
-		InputStream is = new FileInputStream(file);
 
-		// Get the size of the file
-		long length = file.length();
+    public static String readBinaryBase64(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
 
-		// You cannot create an array using a long type.
-		// It needs to be an int type.
-		// Before converting to an int type, check
-		// to ensure that file is not larger than Integer.MAX_VALUE.
-		if (length > Integer.MAX_VALUE) {
-			// File is too large
-		}
+        // Get the size of the file
+        long length = file.length();
 
-		// Create the byte array to hold the data
-		byte[] bytes = new byte[(int) length];
+        // You cannot create an array using a long type.
+        // It needs to be an int type.
+        // Before converting to an int type, check
+        // to ensure that file is not larger than Integer.MAX_VALUE.
+        if (length > Integer.MAX_VALUE) {
+            // File is too large
+        }
 
-		// Read in the bytes
-		int offset = 0;
-		int numRead = 0;
-		while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-			offset += numRead;
-		}
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int) length];
 
-		// Ensure all the bytes have been read in
-		if (offset < bytes.length) {
-			throw new IOException("Could not completely read file " + file.getName());
-		}
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
 
-		// Close the input stream and return bytes
-		is.close();
-		
-		return new String(new Base64().encode(bytes));
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + file.getName());
+        }
+
+        // Close the input stream and return bytes
+        is.close();
+
+        return new String(new Base64().encode(bytes));
     }
 
 }
