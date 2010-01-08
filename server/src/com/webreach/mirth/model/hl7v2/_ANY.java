764a274b1bcfe9c7eae73f2c545bc073e2594cb0
Gerald Bortis <gbortis@nextgen.com>
2010-01-08 01:45:41 +0000

    Added generated vocab jar. Issue: MIRTH-1295

--- server/src/com/webreach/mirth/model/hl7v2/_ANY.java
+++
@@ -1,6 +0,0 @@
-package com.webreach.mirth.model.hl7v2;
-
-public class _ANY extends Segment {
-	protected static String description = "Any HL7 Segment";
-	protected static String name = "ANY";
-}
--- server/src/com/webreach/mirth/model/hl7v2/_Time.java
+++
@@ -1,5 +0,0 @@
-package com.webreach.mirth.model.hl7v2;
-
-public class _Time extends Composite {
-
-}
