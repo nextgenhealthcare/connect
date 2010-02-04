95fd19dc4f8b644fde88209e58631926ccfc230c
Jacob Brauer <jbrauer@nextgen.com>
2010-02-04 22:49:07 +0000

    Switched the Mirth PDF Viewer to use the swinglabs PDFRenderer.jar Issue: MIRTH-1331

--- client/src/com/webreach/mirth/plugins/pdfviewer/PDFViewer.java
+++ client/src/com/webreach/mirth/plugins/pdfviewer/PDFViewer.java
@@ -9,18 +9,13 @@
 
 package com.webreach.mirth.plugins.pdfviewer;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.Point;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 import org.apache.commons.codec.binary.Base64;
 
-import com.adobe.acrobat.Viewer;
 import com.webreach.mirth.model.Attachment;
 import com.webreach.mirth.plugins.AttachmentViewer;
 
@@ -39,62 +34,21 @@ public class PDFViewer extends AttachmentViewer {
     }
 
     public void viewAttachments(List attachmentIds) {
-        // do viewing code
-
-        Frame frame = new Frame("PDF Viewer");
-
-        frame.setLayout(new BorderLayout());
 
         try {
-
-            final Viewer viewer = new Viewer();
-
-            frame.add(viewer, BorderLayout.CENTER);
-
             Attachment attachment = parent.mirthClient.getAttachment((String) attachmentIds.get(0));
             byte[] rawData = attachment.getData();
             byte[] rawPDF = new Base64().decode(rawData);
-            ByteArrayInputStream bis = new ByteArrayInputStream(rawPDF);
 
-            viewer.setDocumentInputStream(bis);
-
-            viewer.setProperty("Default_Page_Layout", "SinglePage");
-            viewer.setProperty("Default_Zoom_Type", "FitPage");
-//			viewer.setProperty("Default_Magnification", "100");
-
-
-            viewer.activate();
-
-            frame.addWindowListener(new WindowAdapter() {
-
-                public void windowClosing(WindowEvent e) {
-
-                    if (viewer != null) {
-
-                        // The deactivate method will ensure that the
-                        // acrobat.properties file is saved
-                        // upon exit.
-
-                        viewer.deactivate();
-                    }
-
-                    e.getWindow().dispose();
-                }
-            });
-
-            frame.setSize(600, 800);
-
-            Dimension dlgSize = frame.getSize();
-            Dimension frmSize = parent.getSize();
-            Point loc = parent.getLocation();
-
-            if ((frmSize.width == 0 && frmSize.height == 0) || (loc.x == 0 && loc.y == 0)) {
-                frame.setLocationRelativeTo(null);
-            } else {
-                frame.setLocation((frmSize.width - dlgSize.width) / 2 + loc.x, (frmSize.height - dlgSize.height) / 2 + loc.y);
-            }
-
-            frame.setVisible(true);
+            File temp = File.createTempFile(attachment.getAttachmentId(), ".pdf");
+            temp.deleteOnExit();
+            
+            OutputStream out = new FileOutputStream(temp);
+            out.write(rawPDF);
+            out.close();
+            
+            new MirthPDFViewer(true, temp);
+            
         } catch (Exception e) {
             parent.alertException(parent, e.getStackTrace(), e.getMessage());
         }
