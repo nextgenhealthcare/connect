1bd0d21c3faeaec43e39b585610a1bb3ecc6c30a
Jacob Brauer <jbrauer@nextgen.com>
2010-03-23 22:55:58 +0000

    Added a respond from option to the Channel Reader.  If wait for response is selected on the channel writer, it will receive this response. Issue: MIRTH-1312

--- client/src/com/webreach/mirth/connectors/vm/ChannelReader.java
+++ client/src/com/webreach/mirth/connectors/vm/ChannelReader.java
@@ -9,18 +9,30 @@
 
 package com.webreach.mirth.connectors.vm;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.swing.DefaultComboBoxModel;
+
+import com.webreach.mirth.client.ui.UIConstants;
+import com.webreach.mirth.client.ui.editors.transformer.TransformerPane;
 import com.webreach.mirth.connectors.ConnectorClass;
+import com.webreach.mirth.model.Channel;
+import com.webreach.mirth.model.Connector;
+import com.webreach.mirth.model.Step;
 
 /**
  * A form that extends from ConnectorClass. All methods implemented are
  * described in ConnectorClass.
  */
 public class ChannelReader extends ConnectorClass {
-
-    private HashMap channelList;
 
     public ChannelReader() {
         name = ChannelReaderProperties.name;
@@ -30,11 +42,20 @@ public class ChannelReader extends ConnectorClass {
     public Properties getProperties() {
         Properties properties = new Properties();
         properties.put(ChannelReaderProperties.DATATYPE, name);
+
+        properties.put(ChannelReaderProperties.CHANNEL_RESPONSE_VALUE, (String) responseFromTransformer.getSelectedItem());
+
         return properties;
     }
 
     public void setProperties(Properties props) {
         resetInvalidProperties();
+
+        updateResponseDropDown();
+
+        if (parent.channelEditPanel.synchronousCheckBox.isSelected()) {
+            responseFromTransformer.setSelectedItem((String) props.getProperty(ChannelReaderProperties.CHANNEL_RESPONSE_VALUE));
+        }
     }
 
     public Properties getDefaults() {
@@ -61,6 +82,92 @@ public class ChannelReader extends ConnectorClass {
         return error;
     }
 
+    public void updateResponseDropDown() {
+        boolean visible = parent.channelEditTasks.getContentPane().getComponent(0).isVisible();
+
+        String selectedItem = (String) responseFromTransformer.getSelectedItem();
+
+        Channel channel = parent.channelEditPanel.currentChannel;
+
+        Set<String> variables = new LinkedHashSet<String>();
+
+        variables.add("None");
+
+        List<Step> stepsToCheck = new ArrayList<Step>();
+        stepsToCheck.addAll(channel.getSourceConnector().getTransformer().getSteps());
+
+        List<String> scripts = new ArrayList<String>();
+
+        for (Connector connector : channel.getDestinationConnectors()) {
+            if (connector.getTransportName().equals("Database Writer")) {
+                if (connector.getProperties().getProperty("useScript").equals(UIConstants.YES_OPTION)) {
+                    scripts.add(connector.getProperties().getProperty("script"));
+                }
+
+            } else if (connector.getTransportName().equals("JavaScript Writer")) {
+                scripts.add(connector.getProperties().getProperty("script"));
+            }
+
+            variables.add(connector.getName());
+            stepsToCheck.addAll(connector.getTransformer().getSteps());
+        }
+
+        Pattern pattern = Pattern.compile(RESULT_PATTERN);
+
+        int i = 0;
+        for (Iterator it = stepsToCheck.iterator(); it.hasNext();) {
+            Step step = (Step) it.next();
+            Map data;
+            data = (Map) step.getData();
+
+            if (step.getType().equalsIgnoreCase(TransformerPane.JAVASCRIPT_TYPE)) {
+                Matcher matcher = pattern.matcher(step.getScript());
+                while (matcher.find()) {
+                    String key = matcher.group(1);
+                    variables.add(key);
+                }
+            } else if (step.getType().equalsIgnoreCase(TransformerPane.MAPPER_TYPE)) {
+                if (data.containsKey(UIConstants.IS_GLOBAL)) {
+                    if (((String) data.get(UIConstants.IS_GLOBAL)).equalsIgnoreCase(UIConstants.IS_GLOBAL_RESPONSE)) {
+                        variables.add((String) data.get("Variable"));
+                    }
+                }
+            }
+        }
+
+        scripts.add(channel.getPreprocessingScript());
+        scripts.add(channel.getPostprocessingScript());
+
+        for (String script : scripts) {
+            if (script != null && script.length() > 0) {
+                Matcher matcher = pattern.matcher(script);
+                while (matcher.find()) {
+                    String key = matcher.group(1);
+                    variables.add(key);
+                }
+            }
+        }
+
+        responseFromTransformer.setModel(new DefaultComboBoxModel(variables.toArray()));
+
+        if (variables.contains(selectedItem)) {
+            responseFromTransformer.setSelectedItem(selectedItem);
+        } else {
+            responseFromTransformer.setSelectedIndex(0);
+        }
+
+        if (!parent.channelEditPanel.synchronousCheckBox.isSelected()) {
+            responseFromTransformer.setEnabled(false);
+            responseFromLabel.setEnabled(false);
+            responseFromTransformer.setSelectedIndex(0);
+        } else {
+            responseFromTransformer.setEnabled(true);
+            responseFromLabel.setEnabled(true);
+        }
+
+        parent.channelEditTasks.getContentPane().getComponent(0).setVisible(visible);
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -70,16 +177,16 @@ public class ChannelReader extends ConnectorClass {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        buttonGroup1 = new javax.swing.ButtonGroup();
-        buttonGroup2 = new javax.swing.ButtonGroup();
-        buttonGroup3 = new javax.swing.ButtonGroup();
-        buttonGroup4 = new javax.swing.ButtonGroup();
-        URL = new javax.swing.JLabel();
+        responseFromTransformer = new com.webreach.mirth.client.ui.components.MirthComboBox();
+        responseFromLabel = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
-        URL.setText("This connector has no properties.");
+        responseFromTransformer.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        responseFromTransformer.setToolTipText("<html>Select None or the name of a destination of this channel that will generate the response to the request.<br>If None is selected, the response will always be \"200 OK\" if the message is successfully processed <br>or \"500 Server Error\" if there is an error processing the message.</html>");
+
+        responseFromLabel.setText("Respond from:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -87,19 +194,22 @@ public class ChannelReader extends ConnectorClass {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(URL)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(responseFromLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(responseFromTransformer, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(24, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(URL)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(responseFromLabel)
+                    .addComponent(responseFromTransformer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel URL;
-    private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.ButtonGroup buttonGroup2;
-    private javax.swing.ButtonGroup buttonGroup3;
-    private javax.swing.ButtonGroup buttonGroup4;
+    private javax.swing.JLabel responseFromLabel;
+    private com.webreach.mirth.client.ui.components.MirthComboBox responseFromTransformer;
     // End of variables declaration//GEN-END:variables
 }
