154eabc18c7aa2af9a378a2882c9d389b5f2730c
Jacob Brauer <jbrauer@nextgen.com>
2010-03-30 18:43:47 +0000

    Removed DOM parsing to get the root node in the XMLAdaptor.  Replaced it with a method that manually parses the XML for the performance benefit. Issue: MIRTH-1377

--- server/src/com/webreach/mirth/server/mule/adaptors/XMLAdaptor.java
+++ server/src/com/webreach/mirth/server/mule/adaptors/XMLAdaptor.java
@@ -9,38 +9,63 @@
 
 package com.webreach.mirth.server.mule.adaptors;
 
-import java.io.StringReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
+import java.util.regex.Pattern;
 
 import com.webreach.mirth.model.MessageObject;
 
 public class XMLAdaptor extends Adaptor {
-	protected void populateMessage(boolean emptyFilterAndTransformer) throws AdaptorException {
-		messageObject.setRawDataProtocol(MessageObject.Protocol.XML);
-		messageObject.setTransformedDataProtocol(MessageObject.Protocol.XML);
-		messageObject.setEncodedDataProtocol(MessageObject.Protocol.XML);
-		messageObject.setType("XML");
-		messageObject.setTransformedData(source);
-		
-		if (emptyFilterAndTransformer) {
-			messageObject.setEncodedData(source);
-		}
-		
-		try {
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			Document xmlDoc = docBuilder.parse(new InputSource(new StringReader(source)));
-			messageObject.setSource(new String());
-			messageObject.setType(xmlDoc.getDocumentElement().getNodeName());
-			messageObject.setVersion("1.0");
-		} catch (Exception e) {
-			handleException(e);
-		}
-		
-	}
+
+    final static Pattern whiteSpacePattern = Pattern.compile("\\s|/");
+
+    protected void populateMessage(boolean emptyFilterAndTransformer) throws AdaptorException {
+        messageObject.setRawDataProtocol(MessageObject.Protocol.XML);
+        messageObject.setTransformedDataProtocol(MessageObject.Protocol.XML);
+        messageObject.setEncodedDataProtocol(MessageObject.Protocol.XML);
+        messageObject.setType("XML");
+        messageObject.setTransformedData(source);
+
+        if (emptyFilterAndTransformer) {
+            messageObject.setEncodedData(source);
+        }
+
+        try {
+            messageObject.setSource(new String());
+            messageObject.setType(getNodeNameFromXMLString(source));
+            messageObject.setVersion("1.0");
+        } catch (Exception e) {
+            handleException(e);
+        }
+
+    }
+
+    /** 
+     * Gets the name of the root node in an XML string
+     */
+    private String getNodeNameFromXMLString(String xml) {
+        xml = xml.trim();
+        
+        // remove processing-instructions or comments
+        while (xml.substring(0, 2).equals("<?") || xml.substring(0, 4).equals("<!--")) {
+            if (xml.substring(0, 2).equals("<?")) {
+                // remove processing-instructions
+                xml = xml.substring(xml.indexOf("?>") + 2).trim();
+            } else {
+                // remove comments
+                xml = xml.substring(xml.indexOf("-->") + 3).trim();
+            }
+        }
+        
+        // get the node name in the form <[prefix]:nodeName[ (..)][/]>
+        String rootNodeName = xml.substring(1, xml.indexOf(">")).trim();
+        
+        // remove attributes (if any)
+        rootNodeName = whiteSpacePattern.split(rootNodeName, 2)[0];
+        
+        // remove prefix (if any)
+        int nsPos = rootNodeName.indexOf(":");
+        if (nsPos > 0)
+            rootNodeName = rootNodeName.substring(nsPos + 1);
+
+        return rootNodeName;
+    }
 }
