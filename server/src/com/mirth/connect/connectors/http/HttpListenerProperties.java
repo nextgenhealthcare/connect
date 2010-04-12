7f51dbbbe750082c6a5efff267b6662eb14099b2
Gerald Bortis <gbortis@nextgen.com>
2010-04-12 22:38:12 +0000

    Added receiver charset property. Issue: MIRTH-1394

--- server/src/com/mirth/connect/connectors/http/HttpListenerProperties.java
+++ server/src/com/mirth/connect/connectors/http/HttpListenerProperties.java
@@ -13,7 +13,8 @@ public class HttpListenerProperties {
     public static final String HTTP_RESPONSE = "receiverResponse";
     public static final String HTTP_INCLUDE_HEADERS = "receiverIncludeHeaders";
     public static final String HTTP_RESPONSE_CONTENT_TYPE = "receiverResponseContentType";
-
+    public static final String HTTP_CHARSET = "receiverCharset";
+    
     public Properties getDefaults() {
         Properties properties = new Properties();
         
@@ -24,6 +25,7 @@ public class HttpListenerProperties {
         properties.put(HTTP_RESPONSE, "None");
         properties.put(HTTP_INCLUDE_HEADERS, "0");
         properties.put(HTTP_RESPONSE_CONTENT_TYPE, "text/plain");
+        properties.put(HTTP_CHARSET, "UTF-8");
         return properties;
     }
 
