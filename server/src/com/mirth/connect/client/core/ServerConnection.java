5d37de351d952038ee6df5a97fe767172b951d60
Gerald Bortis <gbortis@nextgen.com>
2010-08-25 21:39:32 +0000

    Formatted and updated comments.

--- server/src/com/mirth/connect/client/core/ServerConnection.java
+++ server/src/com/mirth/connect/client/core/ServerConnection.java
@@ -30,116 +30,119 @@ import org.apache.commons.io.IOUtils;
 import com.mirth.connect.client.core.ssl.EasySSLProtocolSocketFactory;
 
 public final class ServerConnection {
-	private HttpClient client;
-	private String address;
-	private IdleConnectionTimeoutThread idleConnectionTimeoutThread;
+    private HttpClient client;
+    private String address;
+    private IdleConnectionTimeoutThread idleConnectionTimeoutThread;
 
-	public ServerConnection(String address) {
-	    // default timeout is inifinite
-	    this(address, 0);
-	}
-	
-	public ServerConnection(String address, int timeout) {
+    public ServerConnection(String address) {
+        // Default timeout is inifinite.
+        this(address, 0);
+    }
+
+    public ServerConnection(String address, int timeout) {
         this.address = address;
-        
+
         HttpClientParams httpClientParams = new HttpClientParams();
-    	HttpConnectionManager httpConnectionManager = new SimpleHttpConnectionManager();
-    	httpClientParams.setSoTimeout(timeout);
+        HttpConnectionManager httpConnectionManager = new SimpleHttpConnectionManager();
+        httpClientParams.setSoTimeout(timeout);
         httpConnectionManager.getParams().setConnectionTimeout(10 * 1000);
         httpConnectionManager.getParams().setSoTimeout(timeout);
-        
+
         client = new HttpClient(httpClientParams, httpConnectionManager);
         Protocol mirthHttps = new Protocol("https", new EasySSLProtocolSocketFactory(), 8443);
         Protocol.registerProtocol("https", mirthHttps);
-        
-        // Close connections that have been idle for more than 5 seconds every 5 seconds.
-        // This should help avoid stale connections.
+
+        /*
+         * Close connections that have been idle for more than 5 seconds, every
+         * 5 seconds. This should help avoid stale connections.
+         */
         idleConnectionTimeoutThread = new IdleConnectionTimeoutThread();
         idleConnectionTimeoutThread.addConnectionManager(httpConnectionManager);
         idleConnectionTimeoutThread.setTimeoutInterval(5000);
         idleConnectionTimeoutThread.setConnectionTimeout(5000);
         idleConnectionTimeoutThread.start();
-	}
+    }
 
-	/**
-	 * Executes a POST method on a servlet with a set of parameters.
-	 * 
-	 * @param servletName
-	 *            The name of the servlet.
-	 * @param params
-	 *            An array of NameValuePair objects.
-	 * @return
-	 * @throws ClientException
-	 */
-	public synchronized String executePostMethod(String servletName, NameValuePair[] params) throws ClientException {
-		PostMethod post = null;
+    /**
+     * Executes a POST method on a servlet with a set of parameters.
+     * 
+     * @param servletName
+     *            The name of the servlet.
+     * @param params
+     *            NameValuePair parameters for the request.
+     * @return
+     * @throws ClientException
+     */
+    public synchronized String executePostMethod(String servletName, NameValuePair[] params) throws ClientException {
+        PostMethod post = null;
 
-		try {
-			post = new PostMethod(address + servletName);
-			post.addResponseFooter(new Header("Content-Encoding", "gzip"));
-			post.addResponseFooter(new Header("Accept-Encoding", "gzip,deflate"));
-			post.setRequestBody(params);
+        try {
+            post = new PostMethod(address + servletName);
+            post.addResponseFooter(new Header("Content-Encoding", "gzip"));
+            post.addResponseFooter(new Header("Accept-Encoding", "gzip,deflate"));
+            post.setRequestBody(params);
 
-			int statusCode = client.executeMethod(post);
+            int statusCode = client.executeMethod(post);
 
-			if (statusCode == HttpStatus.SC_NOT_ACCEPTABLE) {
-				throw new VersionMismatchException(post.getStatusLine().toString());
-			} else if (statusCode == HttpStatus.SC_FORBIDDEN) {
-				throw new InvalidLoginException(post.getStatusLine().toString());
-			} else if ((statusCode != HttpStatus.SC_OK) && (statusCode != HttpStatus.SC_MOVED_TEMPORARILY)) {
-				throw new ClientException("method failed: " + post.getStatusLine());
-			}
+            if (statusCode == HttpStatus.SC_NOT_ACCEPTABLE) {
+                throw new VersionMismatchException(post.getStatusLine().toString());
+            } else if (statusCode == HttpStatus.SC_FORBIDDEN) {
+                throw new InvalidLoginException(post.getStatusLine().toString());
+            } else if ((statusCode != HttpStatus.SC_OK) && (statusCode != HttpStatus.SC_MOVED_TEMPORARILY)) {
+                throw new ClientException("method failed: " + post.getStatusLine());
+            }
 
-			return IOUtils.toString(post.getResponseBodyAsStream(), post.getResponseCharSet()).trim();
-		} catch (Exception e) {
-			throw new ClientException(e);
-		} finally {
-			if (post != null) {
-				post.releaseConnection();
-			}
-		}
-	}
-	public synchronized String executeFileUpload(String servletName, NameValuePair[] params, File file) throws ClientException {
-		PostMethod post = null;
+            return IOUtils.toString(post.getResponseBodyAsStream(), post.getResponseCharSet()).trim();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        } finally {
+            if (post != null) {
+                post.releaseConnection();
+            }
+        }
+    }
 
-		try {
-			post = new PostMethod(address + servletName);
-			post.addResponseFooter(new Header("Content-Encoding", "gzip"));
-			post.addResponseFooter(new Header("Accept-Encoding", "gzip,deflate"));
-			
-			// create multipart segment
-			Part[] parts = new Part[params.length + 1];
-			
-			for (int i = 0; i < params.length; i++){
-				parts[i] = new StringPart(params[i].getName(), params[i].getValue());
-			}
-			
-			parts[params.length] = new FilePart(file.getName(), file);
-			post.setRequestEntity(new MultipartRequestEntity(parts, post.getParams()));
-			
-			int statusCode = client.executeMethod(post);
+    public synchronized String executeFileUpload(String servletName, NameValuePair[] params, File file) throws ClientException {
+        PostMethod post = null;
 
-			if (statusCode == HttpStatus.SC_NOT_ACCEPTABLE) {
-				throw new VersionMismatchException(post.getStatusLine().toString());
-			} else if (statusCode == HttpStatus.SC_FORBIDDEN) {
-				throw new InvalidLoginException(post.getStatusLine().toString());
-			} else if ((statusCode != HttpStatus.SC_OK) && (statusCode != HttpStatus.SC_MOVED_TEMPORARILY)) {
-				throw new ClientException("method failed: " + post.getStatusLine());
-			}
+        try {
+            post = new PostMethod(address + servletName);
+            post.addResponseFooter(new Header("Content-Encoding", "gzip"));
+            post.addResponseFooter(new Header("Accept-Encoding", "gzip,deflate"));
 
-			return IOUtils.toString(post.getResponseBodyAsStream(), post.getResponseCharSet()).trim();
-		} catch (Exception e) {
-			throw new ClientException(e);
-		} finally {
-			if (post != null) {
-				post.releaseConnection();
-			}
-		}
-	}
-	
-	public void shutdownTimeoutThread() {
-	    if (idleConnectionTimeoutThread != null) {
-	        idleConnectionTimeoutThread.shutdown();
-	    }
-	}
+            // Create a multipart request from the parameters and the file.
+            Part[] parts = new Part[params.length + 1];
+
+            for (int i = 0; i < params.length; i++) {
+                parts[i] = new StringPart(params[i].getName(), params[i].getValue());
+            }
+
+            parts[params.length] = new FilePart(file.getName(), file);
+            post.setRequestEntity(new MultipartRequestEntity(parts, post.getParams()));
+
+            int statusCode = client.executeMethod(post);
+
+            if (statusCode == HttpStatus.SC_NOT_ACCEPTABLE) {
+                throw new VersionMismatchException(post.getStatusLine().toString());
+            } else if (statusCode == HttpStatus.SC_FORBIDDEN) {
+                throw new InvalidLoginException(post.getStatusLine().toString());
+            } else if ((statusCode != HttpStatus.SC_OK) && (statusCode != HttpStatus.SC_MOVED_TEMPORARILY)) {
+                throw new ClientException("method failed: " + post.getStatusLine());
+            }
+
+            return IOUtils.toString(post.getResponseBodyAsStream(), post.getResponseCharSet()).trim();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        } finally {
+            if (post != null) {
+                post.releaseConnection();
+            }
+        }
+    }
+
+    public void shutdownTimeoutThread() {
+        if (idleConnectionTimeoutThread != null) {
+            idleConnectionTimeoutThread.shutdown();
+        }
+    }
 }
