19a88463c2ef9d7170779934a5f5106ffdb9939b
Gerald Bortis <gbortis@nextgen.com>
2010-09-13 21:51:09 +0000

    Added Java VM name to splash screen.

--- server/src/com/mirth/connect/server/Mirth.java
+++ server/src/com/mirth/connect/server/Mirth.java
@@ -381,7 +381,7 @@ public class Mirth extends Thread {
 
         logger.info("Mirth Connect " + version + " (" + buildDate + ") server successfully started: " + (new Date()).toString());
         logger.info("This product was developed by Mirth Corporation (http://www.mirthcorp.com) and its contributors (c)2005-" + Calendar.getInstance().get(Calendar.YEAR) + ".");
-        logger.info("Running Java " + System.getProperty("java.version") + " on " + System.getProperty("os.name") + " (" + System.getProperty("os.version") + ", " + System.getProperty("os.arch") + ") with charset " + Charset.defaultCharset() + ".");
+        logger.info("Running " + System.getProperty("java.vm.name") + " " + System.getProperty("java.version") + " on " + System.getProperty("os.name") + " (" + System.getProperty("os.version") + ", " + System.getProperty("os.arch") + ") with charset " + Charset.defaultCharset() + ".");        
     }
 
     /**
