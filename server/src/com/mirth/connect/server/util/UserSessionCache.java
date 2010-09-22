9a4d59c7f92822c81160c94ad95b21ff0decae27
Gerald Bortis <gbortis@nextgen.com>
2010-09-22 23:38:36 +0000

    Minor formatting.

--- server/src/com/mirth/connect/server/util/UserSessionCache.java
+++ server/src/com/mirth/connect/server/util/UserSessionCache.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) Mirth Corporation. All rights reserved.
+ * http://www.mirthcorp.com
+ *
+ * The software in this package is published under the terms of the MPL
+ * license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+
 package com.mirth.connect.server.util;
 
 import java.util.Map;
@@ -10,12 +19,11 @@ import org.apache.log4j.Logger;
 
 import com.mirth.connect.model.User;
 
-
 public class UserSessionCache {
     private Logger logger = Logger.getLogger(this.getClass());
     private Map<HttpSession, User> userSessionMap = new ConcurrentHashMap<HttpSession, User>();
     private static UserSessionCache instance = null;
-    
+
     private UserSessionCache() {
 
     }
@@ -29,7 +37,7 @@ public class UserSessionCache {
             return instance;
         }
     }
-    
+
     public void registerSessionForUser(HttpSession session, User user) {
         logger.debug("registering session: user=" + user.getId() + ", session=" + session.getId());
         userSessionMap.put(session, user);
@@ -39,7 +47,7 @@ public class UserSessionCache {
         for (Entry<HttpSession, User> entry : userSessionMap.entrySet()) {
             HttpSession entrySession = entry.getKey();
             User entryUser = entry.getValue();
-            
+
             if (entryUser.getId().equals(user.getId())) {
                 logger.debug("invalidating session: user=" + entryUser.getId() + ", session=" + entrySession.getId());
                 entrySession.removeAttribute("authorized");
