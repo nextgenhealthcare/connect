ba07815a64831642a0a28a424a36a76314fce3e1
Gerald Bortis <gbortis@nextgen.com>
2010-11-11 23:27:38 +0000

    Ran formatter and removed commented code. Issue: MIRTH-1597

--- server/src/com/mirth/connect/model/converters/EDIXMLHandler.java
+++ server/src/com/mirth/connect/model/converters/EDIXMLHandler.java
@@ -13,114 +13,81 @@ import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class EDIXMLHandler extends DefaultHandler {
-	private String segmentDelim;
-	private String elementDelim;
-	private String subelementDelim;
+    private String segmentDelim;
+    private String elementDelim;
+    private String subelementDelim;
 
-	private enum Location {
-		DOCUMENT, SEGMENT, ELEMENT, SUBELEMENT
-	};
+    private enum Location {
+        DOCUMENT, SEGMENT, ELEMENT, SUBELEMENT
+    };
 
-	private Location currentLocation = Location.DOCUMENT;
-	private boolean sawHeader = false;
-	private boolean lastinSubelement = false;
-	private StringBuilder output = new StringBuilder();
+    private Location currentLocation = Location.DOCUMENT;
+    private boolean sawHeader = false;
+    private boolean lastinSubelement = false;
+    private StringBuilder output = new StringBuilder();
 
-	public EDIXMLHandler(String segmentDelim, String elementDelim, String subelementDelim) {
-		super();
-		this.segmentDelim = segmentDelim;
-		this.elementDelim = elementDelim;
-		this.subelementDelim = subelementDelim;
-	}
+    public EDIXMLHandler(String segmentDelim, String elementDelim, String subelementDelim) {
+        super();
+        this.segmentDelim = segmentDelim;
+        this.elementDelim = elementDelim;
+        this.subelementDelim = subelementDelim;
+    }
 
-	// //////////////////////////////////////////////////////////////////
-	// Event handlers.
-	// //////////////////////////////////////////////////////////////////
+    public void startDocument() {
+        currentLocation = Location.DOCUMENT;
+    }
 
-	public void startDocument() {
-		currentLocation = Location.DOCUMENT;
-	}
+    public void endDocument() {
+        currentLocation = Location.DOCUMENT;
+    }
 
-	public void endDocument() {
-		currentLocation = Location.DOCUMENT;
-	}
+    public void startElement(String uri, String name, String qName, Attributes atts) {
+        if (sawHeader == false) {
+            sawHeader = true;
+        } else {
+            if (currentLocation.equals(Location.DOCUMENT)) {
+                output.append(name);
+                currentLocation = Location.SEGMENT;
+                lastinSubelement = false;
+            } else if (currentLocation.equals(Location.SEGMENT)) {
+                output.append(elementDelim);
+                currentLocation = Location.ELEMENT;
+                lastinSubelement = false;
+            } else if (currentLocation.equals(Location.ELEMENT)) {
+                if (lastinSubelement) {
+                    output.append(subelementDelim);
+                }
+                currentLocation = Location.SUBELEMENT;
+                lastinSubelement = true;
+            }
+        }
+    }
 
-	public void startElement(String uri, String name, String qName, Attributes atts) {
-		if (sawHeader == false){
-			sawHeader = true;
-		}else{
-			if (currentLocation.equals(Location.DOCUMENT)){
-				output.append(name);
-				currentLocation = Location.SEGMENT;
-				lastinSubelement = false;
-			}else if (currentLocation.equals(Location.SEGMENT)){
-				output.append(elementDelim);
-				currentLocation = Location.ELEMENT;
-				lastinSubelement = false;
-			}else if (currentLocation.equals(Location.ELEMENT)){
-				if (lastinSubelement){
-					output.append(subelementDelim);
-				}
-				currentLocation = Location.SUBELEMENT;
-				lastinSubelement = true;
-			}
-		}
-	}
+    public void endElement(String uri, String name, String qName) {
+        if (currentLocation.equals(Location.SEGMENT)) {
+            output.append(segmentDelim);
+            currentLocation = Location.DOCUMENT;
+        } else if (currentLocation.equals(Location.ELEMENT)) {
 
-	public void endElement(String uri, String name, String qName) {
-		if (currentLocation.equals(Location.SEGMENT)){
-			//output.deleteCharAt(output.length() - 1);
-			output.append(segmentDelim);
-			currentLocation = Location.DOCUMENT;
-		}else if (currentLocation.equals(Location.ELEMENT)){
-			
-			currentLocation = Location.SEGMENT;
-		}else if (currentLocation.equals(Location.SUBELEMENT)){
-			
-			currentLocation = Location.ELEMENT;
-		}else if (currentLocation.equals(Location.DOCUMENT)){
-			
-		}
-		
-	}
+            currentLocation = Location.SEGMENT;
+        } else if (currentLocation.equals(Location.SUBELEMENT)) {
 
-	public void characters(char ch[], int start, int length) {
-		output.append(ch, start, length);
-		/*
-		for (int i = start; i < start + length; i++) {
-			switch (ch[i]) {
-				/*
-			case '\\':
-				output.append("\\\\");
-				break;
-			case '"':
-				output.append("\\\"");
-				break;
-			case '\n':
-				output.append("\n");
-				break;
-			case '\r':
-				output.append("\r");
-				break;
-			case '\t':
-				output.append("\t");
-				break;
-				
-			default:
-				output.append(ch[i]);
-				break;
-			}
-		}
-		*/
-		//System.out.print("\"\n");
-	}
+            currentLocation = Location.ELEMENT;
+        } else if (currentLocation.equals(Location.DOCUMENT)) {
 
-	public StringBuilder getOutput() {
-		return output;
-	}
+        }
 
-	public void setOutput(StringBuilder output) {
-		this.output = output;
-	}
+    }
 
+    public void characters(char ch[], int start, int length) {
+        output.append(ch, start, length);
+    }
+
+    public StringBuilder getOutput() {
+        return output;
+    }
+
+    public void setOutput(StringBuilder output) {
+        this.output = output;
+    }
 }
