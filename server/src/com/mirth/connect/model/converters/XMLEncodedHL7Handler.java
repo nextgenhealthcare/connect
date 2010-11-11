2e21fdf14a1d24ffecb9c1a1c69ab59c2a27161c
Gerald Bortis <gbortis@nextgen.com>
2010-11-11 02:06:21 +0000

    Fixed bug where missing fields and components were not being added. Optimized start element. Issue: MIRTH-1636

--- server/src/com/mirth/connect/model/converters/XMLEncodedHL7Handler.java
+++ server/src/com/mirth/connect/model/converters/XMLEncodedHL7Handler.java
@@ -9,7 +9,9 @@
 
 package com.mirth.connect.model.converters;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.log4j.Logger;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -28,7 +30,8 @@ public class XMLEncodedHL7Handler extends DefaultHandler {
     private boolean inElement = false;
 
     private int previousDelimeterCount = -1;
-    private String previousFieldName;
+    private String[] previousFieldNameArray;
+    private String[] previousComponentNameArray;
 
     private StringBuilder output = new StringBuilder();
 
@@ -53,16 +56,24 @@ public class XMLEncodedHL7Handler extends DefaultHandler {
         logger.trace("starting element: " + localName);
         inElement = true;
 
+        String[] localNameArray = StringUtils.split(localName, ID_DELIMETER);
+
         /*
          * Skip the root element, MSH.1, and MSH.2 since those don't have any
          * data that we care about.
          */
-        if (localName.equals(ER7Reader.MESSAGE_ROOT_ID)) {
+        if ((localNameArray.length == 1) && (localNameArray[0].equals(ER7Reader.MESSAGE_ROOT_ID))) {
             return;
-        } else if (localName.equals("MSH.1") || localName.equals("BHS.1") || localName.equals("FHS.1")) {
-            return;
-        } else if (localName.equals("MSH.2") || localName.equals("BHS.2") || localName.equals("FHS.2")) {
-            return;
+        } else if (localNameArray.length == 2) {
+            /*
+             * This awesome piece of code returns if it is an [M|S|H].[1|2]
+             */
+            if ((localNameArray[0].length() == 3) && (localNameArray[0].charAt(1) == 'S') && (localNameArray[0].charAt(2) == 'H') && ((localNameArray[0].charAt(0) == 'M') || (localNameArray[0].charAt(0) == 'B') || (localNameArray[0].charAt(0) == 'F'))) {
+                if ((localNameArray[1].length() == 1) && (localNameArray[1].charAt(0) == '1' || localNameArray[1].charAt(0) == '2')) {
+                    previousFieldNameArray = localNameArray;
+                    return;
+                }
+            }
         }
 
         /*
@@ -70,16 +81,54 @@ public class XMLEncodedHL7Handler extends DefaultHandler {
          * a repetition, so we remove the last separator that was added and
          * append to repetition separator.
          */
-        if (localName.equals(previousFieldName)) {
+        if (ArrayUtils.isEquals(localNameArray, previousFieldNameArray)) {
             output.deleteCharAt(output.length() - 1);
             output.append(repetitionSeparator);
             return;
         }
 
-        int currentDelimeterCount = StringUtils.countMatches(localName, ID_DELIMETER);
+        /*
+         * To find the delimeter count we are splitting the element name by the
+         * ID delimeter.
+         */
+        int currentDelimeterCount = localNameArray.length - 1;
 
         if (currentDelimeterCount == 1) {
-            previousFieldName = localName;
+            /*
+             * This will add missing fields if any (ex. between OBX.1 and
+             * OBX.5).
+             */
+            int previousFieldId = 0;
+
+            if (previousFieldNameArray != null) {
+                previousFieldId = NumberUtils.toInt(previousFieldNameArray[1]);
+            }
+
+            int currentFieldId = NumberUtils.toInt(localNameArray[1]);
+
+            for (int i = 1; i < (currentFieldId - previousFieldId); i++) {
+                output.append(fieldSeparator);
+            }
+
+            previousFieldNameArray = localNameArray;
+        } else if (currentDelimeterCount == 2) {
+            /*
+             * This will add missing components if any (ex. between OBX.1.1 and
+             * OBX.1.5).
+             */
+            int previousComponentId = 0;
+
+            if (previousComponentNameArray != null) {
+                previousComponentId = NumberUtils.toInt(previousComponentNameArray[2]);
+            }
+
+            int currentComponentId = NumberUtils.toInt(localNameArray[2]);
+
+            for (int i = 1; i < (currentComponentId - previousComponentId); i++) {
+                output.append(componentSeparator);
+            }
+
+            previousComponentNameArray = localNameArray;
         }
 
         /*
@@ -96,7 +145,9 @@ public class XMLEncodedHL7Handler extends DefaultHandler {
              * row with only one field don't trigger a repetition character.
              * (i.e. NTE|1<CR>NTE|2)
              */
-            previousFieldName = null;
+            previousFieldNameArray = null;
+        } else if (currentDelimeterCount == 1) {
+            previousComponentNameArray = null;
         }
     }
 
