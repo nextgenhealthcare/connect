28481090bbd5fdb70416db30f1b7b4a6f49dd0a9
Gerald Bortis <gbortis@nextgen.com>
2011-02-10 23:54:10 +0000

    Changed date to datetime Issue: MIRTH-1664

--- client/src/com/mirth/connect/client/ui/browsers/event/EventBrowser.java
+++ client/src/com/mirth/connect/client/ui/browsers/event/EventBrowser.java
@@ -129,7 +129,7 @@ public class EventBrowser extends javax.swing.JPanel {
 
                 tableData[i][0] = systemEvent.getId();
 
-                Calendar calendar = systemEvent.getDate();
+                Calendar calendar = systemEvent.getDateTime();
 
                 tableData[i][1] = String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS:%1$tL", calendar);
 
