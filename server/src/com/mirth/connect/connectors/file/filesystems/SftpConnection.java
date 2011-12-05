037f7c4ba5332ecdc7412efdbb7f540df0f3af36
Gerald Bortis <gbortis@nextgen.com>
2011-12-05 19:51:26 +0000

    Fixed imports Issue: MIRTH-2014

--- server/src/com/mirth/connect/connectors/file/filesystems/SftpConnection.java
+++ server/src/com/mirth/connect/connectors/file/filesystems/SftpConnection.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Vector;
 
 import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
