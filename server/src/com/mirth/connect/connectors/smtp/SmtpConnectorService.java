97038049b512eb4630a1398892afb30c462d47b7
Jacob Brauer <jbrauer@nextgen.com>
2012-01-16 19:59:36 +0000

    Added error catching with friendly errors for invalid ports, to/from email addresses, and invalid email messages. Issue: MIRTH-2056

--- server/src/com/mirth/connect/connectors/smtp/SmtpConnectorService.java
+++ server/src/com/mirth/connect/connectors/smtp/SmtpConnectorService.java
@@ -15,27 +15,34 @@ public class SmtpConnectorService implements ConnectorService {
     public Object invoke(String method, Object object, String sessionId) throws Exception {
         if (method.equals("sendTestEmail")) {
             Properties props = (Properties) object;
-            
+
             String host = props.getProperty(SmtpSenderProperties.SMTP_HOST);
-            int port = Integer.parseInt(props.getProperty(SmtpSenderProperties.SMTP_PORT));
+
+            int port = -1;
+            try {
+                port = Integer.parseInt(props.getProperty(SmtpSenderProperties.SMTP_PORT));
+            } catch (NumberFormatException e) {
+                return new ConnectionTestResponse(ConnectionTestResponse.Type.FAILURE, "Invalid port: \"" + props.getProperty(SmtpSenderProperties.SMTP_PORT) + "\"");
+            }
+
             String secure = props.getProperty(SmtpSenderProperties.SMTP_SECURE);
-            
+
             boolean authentication = false;
-            
+
             if ("1".equals(props.getProperty(SmtpSenderProperties.SMTP_AUTHENTICATION))) {
                 authentication = true;
             }
-            
+
             String username = props.getProperty(SmtpSenderProperties.SMTP_USERNAME);
             String password = props.getProperty(SmtpSenderProperties.SMTP_PASSWORD);
             String to = props.getProperty(SmtpSenderProperties.SMTP_TO);
             String from = props.getProperty(SmtpSenderProperties.SMTP_FROM);
-            
+
             Email email = new SimpleEmail();
             email.setDebug(true);
             email.setHostName(host);
             email.setSmtpPort(port);
-            
+
             if ("SSL".equalsIgnoreCase(secure)) {
                 email.setSSL(true);
             } else if ("TLS".equalsIgnoreCase(secure)) {
@@ -47,13 +54,14 @@ public class SmtpConnectorService implements ConnectorService {
             }
 
             email.setSubject("Mirth Connect Test Email");
-            email.addTo(to);
-            email.setFrom(from);
-            email.setMsg("Receipt of this email confirms that mail originating from this Mirth Connect Server is capable of reaching its intended destination.\n\nSMTP Configuration:\n- Host: " + host + "\n- Port: " + port);
-            
+
             try {
+                email.addTo(to);
+                email.setFrom(from);
+                email.setMsg("Receipt of this email confirms that mail originating from this Mirth Connect Server is capable of reaching its intended destination.\n\nSMTP Configuration:\n- Host: " + host + "\n- Port: " + port);
+
                 email.send();
-                return new ConnectionTestResponse(ConnectionTestResponse.Type.SUCCESS, "Sucessfully sent test email.");
+                return new ConnectionTestResponse(ConnectionTestResponse.Type.SUCCESS, "Sucessfully sent test email to: " + to);
             } catch (EmailException e) {
                 return new ConnectionTestResponse(ConnectionTestResponse.Type.FAILURE, e.getMessage());
             }
