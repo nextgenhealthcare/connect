fd494dd1fb81638ae477c87554b83fc9b20bd15b
Nick Rupley <nrupley@nextgen.com>
2012-11-28 22:01:14 +0000

    Removed the unescape method from DelimitedProperties, because it is now included in StringUtil (it's used by ER7Serializer now too). Issue: MIRTH-2272

--- server/src/com/mirth/connect/model/converters/delimited/DelimitedProperties.java
+++ server/src/com/mirth/connect/model/converters/delimited/DelimitedProperties.java
@@ -9,6 +9,8 @@
 
 package com.mirth.connect.model.converters.delimited;
 
+import static com.mirth.connect.util.StringUtil.unescape;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -340,55 +342,6 @@ public class DelimitedProperties {
 
 	public void setBatchSplitByRecord(boolean batchSplitByRecord) {
 		this.batchSplitByRecord = batchSplitByRecord;
-	}
-
-	// Four ways to specify character values and string values
-	// 1. Literal
-	// 2. Quoted literal (turns off escape processing except for standard escape sequences)
-	// 3. Standard escape sequences (e.g. \n, \r, \t)
-	// 4. Hex notation (e.g. 0xyy)
-	private String unescape(String s) {
-		
-		// If not set, return the string
-		if (!isSet(s)) {
-			return s;
-		}
-		
-		// If the value is bracket delimited in double quotes, remove the quotes and treat the rest as a literal
-		if (s.length() >= 2 && s.substring(0,1).equals("\"") && s.substring(s.length()-1,s.length()).equals("\"")) {
-			return s.substring(1, s.length() - 1);
-		}
-		
-		// Standard escape sequence substitutions for non-printable characters (excludes printable characters: \ " ')
-		s = s.replace("\\b", "\b");
-		s = s.replace("\\t", "\t");
-		s = s.replace("\\n", "\n");
-		s = s.replace("\\f", "\f");
-		s = s.replace("\\r", "\r");
-		
-		// Substitute hex sequences with single character (e.g. 0x0a -> \n)
-		int n=0;
-		while ((n = s.indexOf("0x", n)) != -1 && s.length() >= n + 4) {
-			char ch;
-			try {
-				ch = (char) Integer.parseInt(s.substring(n+2, n+4), 16);
-			}
-			catch (NumberFormatException e) {
-				n += 2;
-				continue;
-			}
-			if (n + 4 < s.length()) {
-				s = s.substring(0, n) + ch + s.substring(n + 4);
-			}
-			else {
-				s = s.substring(0, n) + ch;
-				break;
-			}
-				
-			n++;
-		}
-		
-		return s;
 	}
 	
 	private boolean validXMLElementName(String s) {
