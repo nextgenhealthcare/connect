943d5c85691fe02516f51c2511818d8dc055cda3
Nick Rupley <nrupley@nextgen.com>
2012-11-28 21:49:08 +0000

    Commented out references to HL7v2AutoResponder until that is committed. Issue: MIRTH-2277

--- server/src/com/mirth/connect/model/converters/AutoResponderFactory.java
+++ server/src/com/mirth/connect/model/converters/AutoResponderFactory.java
@@ -12,29 +12,27 @@ package com.mirth.connect.model.converters;
 import java.util.Map;
 
 import com.mirth.connect.donkey.model.message.AutoResponder;
-import com.mirth.connect.model.converters.hl7v2.HL7v2AutoResponder;
 
 public class AutoResponderFactory {
 
     public static AutoResponder getAutoResponder(String dataType, Map<?, ?> properties) {
-        if (dataType.equals(DataTypeFactory.HL7V2)) {
-            return new HL7v2AutoResponder(properties);
-        } /*
-           * else if (dataType.equals(DataTypeFactory.HL7V3)) {
-           * return new HL7V3AutoResponder(properties);
-           * } else if (dataType.equals(DataTypeFactory.X12)) {
-           * return new X12AutoResponder(properties);
-           * } else if (dataType.equals(DataTypeFactory.EDI)) {
-           * return new EDIAutoResponder(properties);
-           * } else if (dataType.equals(DataTypeFactory.NCPDP)) {
-           * return new NCPDPAutoResponder(properties);
-           * } else if (dataType.equals(DataTypeFactory.DICOM)) {
-           * return new DICOMAutoResponder(properties);
-           * } else if (dataType.equals(DataTypeFactory.DELIMITED)) {
-           * return new DelimitedAutoResponder(properties);
-           * }
-           */else {
-            return new DefaultAutoResponder();
-        }
+//        if (dataType.equals(DataTypeFactory.HL7V2)) {
+//            return new HL7v2AutoResponder(properties);
+//        } else if (dataType.equals(DataTypeFactory.HL7V3)) {
+//            return new HL7V3AutoResponder(properties);
+//        } else if (dataType.equals(DataTypeFactory.X12)) {
+//            return new X12AutoResponder(properties);
+//        } else if (dataType.equals(DataTypeFactory.EDI)) {
+//            return new EDIAutoResponder(properties);
+//        } else if (dataType.equals(DataTypeFactory.NCPDP)) {
+//            return new NCPDPAutoResponder(properties);
+//        } else if (dataType.equals(DataTypeFactory.DICOM)) {
+//            return new DICOMAutoResponder(properties);
+//        } else if (dataType.equals(DataTypeFactory.DELIMITED)) {
+//            return new DelimitedAutoResponder(properties);
+//        } else {
+//            return new DefaultAutoResponder();
+//        }
+        return new DefaultAutoResponder();
     }
 }
