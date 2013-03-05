67cb1bebe226a6a5778be796543870b84b47d19c
Nick Rupley <nrupley@nextgen.com>
2013-03-05 20:31:31 +0000

    MIRTH-2029: Fixed bug that caused null or empty bodies in the SMTP Sender to cause an NPE when the channel attempts to dispatch.

--- server/src/com/mirth/connect/connectors/smtp/SmtpMessageDispatcher.java
+++ server/src/com/mirth/connect/connectors/smtp/SmtpMessageDispatcher.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) Mirth Corporation. All rights reserved.
  * http://www.mirthcorp.com
- *
+ * 
  * The software in this package is published under the terms of the MPL
  * license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
@@ -125,10 +125,12 @@ public class SmtpMessageDispatcher extends AbstractMessageDispatcher {
 
             String body = replacer.replaceValues(connector.getBody(), mo);
 
-            if (connector.isHtml()) {
-                ((HtmlEmail) email).setHtmlMsg(body);
-            } else {
-                email.setMsg(body);
+            if (StringUtils.isNotEmpty(body)) {
+                if (connector.isHtml()) {
+                    ((HtmlEmail) email).setHtmlMsg(body);
+                } else {
+                    email.setMsg(body);
+                }
             }
 
             /*
