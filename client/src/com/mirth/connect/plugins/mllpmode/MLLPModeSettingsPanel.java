7011edc9d10010eeddd65b0f6d0becc82c0fda2c
Jacob Brauer <jbrauer@nextgen.com>
2013-04-04 23:24:08 +0000

    Updated license header.

--- client/src/com/mirth/connect/plugins/mllpmode/MLLPModeSettingsPanel.java
+++ client/src/com/mirth/connect/plugins/mllpmode/MLLPModeSettingsPanel.java
@@ -1,7 +1,12 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) Mirth Corporation. All rights reserved.
+ * http://www.mirthcorp.com
+ * 
+ * The software in this package is published under the terms of the MPL
+ * license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
  */
+
 package com.mirth.connect.plugins.mllpmode;
 
 import java.awt.event.ActionListener;
--- server/src/com/mirth/connect/plugins/datatypes/delimited/DelimitedBatchReader.java
+++ server/src/com/mirth/connect/plugins/datatypes/delimited/DelimitedBatchReader.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) Mirth Corporation. All rights reserved.
+ * http://www.mirthcorp.com
+ * 
+ * The software in this package is published under the terms of the MPL
+ * license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+
 package com.mirth.connect.plugins.datatypes.delimited;
 
 import java.io.BufferedReader;
