04acfa5fdafd3547d6dec55362e1e26eb7bf7d5a
Nick Rupley <nrupley@nextgen.com>
2013-04-04 20:02:54 +0000

    MIRTH-2392: Added the Rhino stacktrace into the MirthJavascriptTransformerException message.

--- server/src/com/mirth/connect/server/MirthJavascriptTransformerException.java
+++ server/src/com/mirth/connect/server/MirthJavascriptTransformerException.java
@@ -15,60 +15,60 @@ import com.mirth.connect.model.Channel;
 import com.mirth.connect.server.controllers.ControllerFactory;
 
 public class MirthJavascriptTransformerException extends Exception {
-	private static final long serialVersionUID = 1L;
-	String message;
-	Exception exception;
-	private String lineSeperator = System.getProperty("line.separator");
+    private static final long serialVersionUID = 1L;
+    String message;
+    Exception exception;
+    private String lineSeperator = System.getProperty("line.separator");
 
-	public MirthJavascriptTransformerException(RhinoException e, String channelId, String connectorName, int lineOffset, String scriptSource, String sourceCode) {
-		super(e);
-		exception = e;
+    public MirthJavascriptTransformerException(RhinoException e, String channelId, String connectorName, int lineOffset, String scriptSource, String sourceCode) {
+        super(e);
+        exception = e;
 
-		StringBuilder sBuilder = new StringBuilder();
-		sBuilder.append(lineSeperator);
-		if (channelId != null) {
-			Channel channel = ControllerFactory.getFactory().createChannelController().getDeployedChannelById(channelId);
-			if (channel != null) {
-				sBuilder.append("CHANNEL:\t").append(channel.getName());
-				sBuilder.append(lineSeperator);
-			}
-		}
-		if (connectorName != null) {
-			sBuilder.append("CONNECTOR:\t" + connectorName);
-			sBuilder.append(lineSeperator);
-		}
-		if (scriptSource != null){
-			sBuilder.append("SCRIPT SOURCE:\t" + scriptSource);
-			sBuilder.append(lineSeperator);
-		}
-        if(sourceCode != null){
+        StringBuilder sBuilder = new StringBuilder();
+        sBuilder.append(lineSeperator);
+        if (channelId != null) {
+            Channel channel = ControllerFactory.getFactory().createChannelController().getDeployedChannelById(channelId);
+            if (channel != null) {
+                sBuilder.append("CHANNEL:\t").append(channel.getName());
+                sBuilder.append(lineSeperator);
+            }
+        }
+        if (connectorName != null) {
+            sBuilder.append("CONNECTOR:\t" + connectorName);
+            sBuilder.append(lineSeperator);
+        }
+        if (scriptSource != null) {
+            sBuilder.append("SCRIPT SOURCE:\t" + scriptSource);
+            sBuilder.append(lineSeperator);
+        }
+        if (sourceCode != null) {
             sBuilder.append("SOURCE CODE:\t" + sourceCode);
             sBuilder.append(lineSeperator);
         }
-		sBuilder.append("LINE NUMBER:\t").append(e.lineNumber() - lineOffset);
-		sBuilder.append(lineSeperator);
-		if (e.lineSource() != null){
-			sBuilder.append("LINE SOURCE:\t").append(e.lineSource());
-			sBuilder.append(lineSeperator);
-		}
-		sBuilder.append("DETAILS:\t" + e.details());
-		message = sBuilder.toString();
-	}
+        sBuilder.append("LINE NUMBER:\t").append(e.lineNumber() - lineOffset);
+        sBuilder.append(lineSeperator);
+        if (e.lineSource() != null) {
+            sBuilder.append("LINE SOURCE:\t").append(e.lineSource());
+            sBuilder.append(lineSeperator);
+        }
+        sBuilder.append("DETAILS:\t" + e.details() + '\n' + e.getScriptStackTrace().replaceAll("\\s+$", ""));
+        message = sBuilder.toString();
+    }
 
-	public Throwable getCause() {
-		return exception.getCause();
-	}
+    public Throwable getCause() {
+        return exception.getCause();
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public String getLocalizedMessage() {
-		return message;
-	}
+    public String getLocalizedMessage() {
+        return message;
+    }
 
-	public StackTraceElement[] getStackTrace() {
-		return exception.getStackTrace();
-	}
+    public StackTraceElement[] getStackTrace() {
+        return exception.getStackTrace();
+    }
 
 }
