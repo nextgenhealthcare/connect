8916aa4644a3c5b393c85a89f0b00b30a5e4401d
Nick Rupley <nrupley@nextgen.com>
2013-07-19 17:19:54 +0000

    MIRTH-2789: Fixed concurrent modification exception in ServerLogProvider that occurs occasionally when sending a large amount of messages to the server log. The issue was that two instances of ServerLogProvider were being created rather than one, so the synchronization on the newServerLogReceived and invoke methods wasn't working as intended.

--- server/src/com/mirth/connect/plugins/serverlog/ArrayAppender.java
+++ server/src/com/mirth/connect/plugins/serverlog/ArrayAppender.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) Mirth Corporation. All rights reserved.
  * http://www.mirthcorp.com
- *
+ * 
  * The software in this package is published under the terms of the MPL
  * license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
@@ -13,19 +13,18 @@ import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.ErrorCode;
 import org.apache.log4j.spi.LoggingEvent;
 
-
 public class ArrayAppender extends AppenderSkeleton {
-	private ServerLogProvider serverLogProvider;
+    private ServerLogProvider serverLogProvider;
 
-	public ArrayAppender() {
-		serverLogProvider = new ServerLogProvider();
-	}
+    public ArrayAppender(ServerLogProvider serverLogProvider) {
+        this.serverLogProvider = serverLogProvider;
+    }
 
-	protected void append(LoggingEvent loggingEvent) {
-		if (this.layout == null) {
-			errorHandler.error("No layout for appender " + name, null, ErrorCode.MISSING_LAYOUT);
-			return;
-		}
+    protected void append(LoggingEvent loggingEvent) {
+        if (this.layout == null) {
+            errorHandler.error("No layout for appender " + name, null, ErrorCode.MISSING_LAYOUT);
+            return;
+        }
 
         // get the complete stack trace, if applicable.
         if (loggingEvent.getThrowableStrRep() != null) {
@@ -38,18 +37,18 @@ public class ArrayAppender extends AppenderSkeleton {
             serverLogProvider.newServerLogReceived(this.layout.format(loggingEvent) + logText.toString());
         } else {
             // pass the new log message to ServerLogProvider.
-		    serverLogProvider.newServerLogReceived(this.layout.format(loggingEvent));
+            serverLogProvider.newServerLogReceived(this.layout.format(loggingEvent));
         }
-	}
+    }
 
-	public boolean requiresLayout() {
-		return true;
-	}
+    public boolean requiresLayout() {
+        return true;
+    }
 
-	public void close() {
-		// clean up and set the boolean 'closed' to true to indiciate that this
-		// appender has been shut down.
-		serverLogProvider = null;
-		closed = true;
-	}
+    public void close() {
+        // clean up and set the boolean 'closed' to true to indiciate that this
+        // appender has been shut down.
+        serverLogProvider = null;
+        closed = true;
+    }
 }
