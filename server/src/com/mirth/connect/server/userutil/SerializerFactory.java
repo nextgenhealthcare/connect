2f10e6bfa17879816292dba23c391fcb01bf6103
Nick Rupley <nrupley@nextgen.com>
2013-08-01 21:54:42 +0000

    MIRTH-2819: Fixed NullPointerException being thrown from SerializerFactory for data types that don't have deserialization properties (e.g. HL7v3).

--- server/src/com/mirth/connect/server/userutil/SerializerFactory.java
+++ server/src/com/mirth/connect/server/userutil/SerializerFactory.java
@@ -38,8 +38,13 @@ public class SerializerFactory {
             }
 
             SerializerProperties properties = plugin.getDefaultProperties().getSerializerProperties();
-            properties.getSerializationProperties().setProperties(serializationPropertiesMap);
-            properties.getDeserializationProperties().setProperties(deserializationPropertiesMap);
+
+            if (properties.getSerializationProperties() != null) {
+                properties.getSerializationProperties().setProperties(serializationPropertiesMap);
+            }
+            if (properties.getDeserializationProperties() != null) {
+                properties.getDeserializationProperties().setProperties(deserializationPropertiesMap);
+            }
 
             return plugin.getSerializer(properties);
         } else {
@@ -49,7 +54,7 @@ public class SerializerFactory {
 
     public static Map<String, Object> getDefaultSerializationProperties(String dataType) {
         DataTypeServerPlugin plugin = extensionController.getDataTypePlugins().get(dataType);
-        if (plugin != null) {
+        if (plugin != null && plugin.getDefaultProperties().getSerializationProperties() != null) {
             return plugin.getDefaultProperties().getSerializationProperties().getProperties();
         } else {
             return null;
@@ -58,15 +63,14 @@ public class SerializerFactory {
 
     public static Map<String, Object> getDefaultDeserializationProperties(String dataType) {
         DataTypeServerPlugin plugin = extensionController.getDataTypePlugins().get(dataType);
-        if (plugin != null) {
+        if (plugin != null && plugin.getDefaultProperties().getDeserializationProperties() != null) {
             return plugin.getDefaultProperties().getDeserializationProperties().getProperties();
         } else {
             return null;
         }
     }
-    
+
     @Deprecated
-    
     // TODO: Remove in 3.1
     private static IXMLSerializer getHL7SerializerQuietly(boolean useStrictParser, boolean useStrictValidation, boolean handleRepetitions, boolean convertLFtoCR, boolean handleSubcomponents) {
         String dataType = "HL7V2";
@@ -77,21 +81,21 @@ public class SerializerFactory {
         serializationPropertiesMap.put("useStrictValidation", useStrictValidation);
         serializationPropertiesMap.put("segmentDelimiter", "\\r");
         serializationPropertiesMap.put("convertLineBreaks", convertLFtoCR);
-        
+
         Map<String, Object> deserializationPropertiesMap = getDefaultDeserializationProperties(dataType);
         deserializationPropertiesMap.put("useStrictParser", useStrictParser);
         deserializationPropertiesMap.put("useStrictValidation", useStrictValidation);
-        
+
         return getSerializer(dataType, serializationPropertiesMap, deserializationPropertiesMap);
     }
-    
+
     @Deprecated
     // TODO: Remove in 3.1
     public static IXMLSerializer getHL7Serializer(boolean useStrictParser, boolean useStrictValidation, boolean handleRepetitions, boolean convertLFtoCR, boolean handleSubcomponents) {
         logger.error("The getHL7Serializer(useStrictParser, useStrictValidation, handleRepetitions, convertLFtoCR, handleSubcomponents) method is deprecated and will soon be removed. Please use the \"Convert HL7 v2.x\" templates from the References tab instead. Look at the tooltips to see the available property keys.");
         return getHL7SerializerQuietly(useStrictParser, useStrictValidation, handleRepetitions, convertLFtoCR, handleSubcomponents);
     }
-    
+
     @Deprecated
     // TODO: Remove in 3.1
     public static IXMLSerializer getHL7Serializer(boolean useStrictParser, boolean useStrictValidation, boolean handleRepetitions, boolean convertLFtoCR) {
@@ -119,34 +123,34 @@ public class SerializerFactory {
         logger.error("The getHL7Serializer() method is deprecated and will soon be removed. Please use the \"Convert HL7 v2.x\" templates from the References tab instead. Look at the tooltips to see the available property keys.");
         return getHL7SerializerQuietly(false, false, false, true, false);
     }
-    
+
     @Deprecated
     // TODO: Remove in 3.1
     public static IXMLSerializer getX12Serializer(boolean inferDelimiters) {
         logger.error("The getX12Serializer(inferDelimiters) method is deprecated and will soon be removed. Please use the \"Convert EDI / X12\" templates from the References tab instead. Look at the tooltips to see the available property keys.");
-        
+
         String dataType = "EDI/X12";
         Map<String, Object> serializationPropertiesMap = getDefaultSerializationProperties(dataType);
         serializationPropertiesMap.put("inferX12Delimiters", inferDelimiters);
-        
+
         return getSerializer(dataType, serializationPropertiesMap, null);
     }
-    
+
     @Deprecated
     // TODO: Remove in 3.1
     public static IXMLSerializer getEDISerializer(String segmentDelim, String elementDelim, String subelementDelim) {
         logger.error("The getEDISerializer(segmentDelim, elementDelim, subelementDelim) method is deprecated and will soon be removed. Please use the \"Convert EDI / X12\" templates from the References tab instead. Look at the tooltips to see the available property keys.");
-        
+
         String dataType = "EDI/X12";
         Map<String, Object> serializationPropertiesMap = getDefaultSerializationProperties(dataType);
         serializationPropertiesMap.put("segmentDelimiter", segmentDelim);
         serializationPropertiesMap.put("elementDelimiter", elementDelim);
         serializationPropertiesMap.put("subelementDelimiter", subelementDelim);
         serializationPropertiesMap.put("inferX12Delimiters", false);
-        
+
         return getSerializer(dataType, serializationPropertiesMap, null);
     }
-    
+
     @Deprecated
     // TODO: Remove in 3.1
     private static IXMLSerializer getNCPDPSerializerQuietly(String segmentDelim, String groupDelim, String fieldDelim, boolean useStrictValidation) {
@@ -155,23 +159,23 @@ public class SerializerFactory {
         serializationPropertiesMap.put("segmentDelimiter", segmentDelim);
         serializationPropertiesMap.put("groupDelimiter", groupDelim);
         serializationPropertiesMap.put("fieldDelimiter", fieldDelim);
-        
+
         Map<String, Object> deserializationPropertiesMap = getDefaultDeserializationProperties(dataType);
         deserializationPropertiesMap.put("segmentDelimiter", segmentDelim);
         deserializationPropertiesMap.put("groupDelimiter", groupDelim);
         deserializationPropertiesMap.put("fieldDelimiter", fieldDelim);
         deserializationPropertiesMap.put("useStrictValidation", useStrictValidation);
-        
+
         return getSerializer(dataType, serializationPropertiesMap, deserializationPropertiesMap);
     }
-    
+
     @Deprecated
     // TODO: Remove in 3.1
     public static IXMLSerializer getNCPDPSerializer(String segmentDelim, String groupDelim, String fieldDelim, boolean useStrictValidation) {
         logger.error("The getNCPDPSerializer(segmentDelim, groupDelim, fieldDelim, useStrictValidation) method is deprecated and will soon be removed. Please use the \"Convert NCPDP\" templates from the References tab instead. Look at the tooltips to see the available property keys.");
         return getNCPDPSerializerQuietly(segmentDelim, groupDelim, fieldDelim, useStrictValidation);
     }
-    
+
     @Deprecated
     // TODO: Remove in 3.1
     public static IXMLSerializer getNCPDPSerializer(String segmentDelim, String groupDelim, String fieldDelim) {
