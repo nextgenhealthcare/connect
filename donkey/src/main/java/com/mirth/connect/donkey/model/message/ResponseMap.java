376bec8cf37fa4bde1e61d4416934ef4e44b9862
Nick Rupley <nrupley@nextgen.com>
2013-08-12 16:32:25 +0000

    MIRTH-2673: Fixed issue that caused objects placed in the response map in a filter/transformer to not get persisted afterwards. Instead of acting as a standalone HashMap, ResponseMap just implements Map and delegates all methods to the Map passed into the constructor. That way when a user calls put() in JavaScript, it affects the original map from the ConnectorMessage.

--- donkey/src/main/java/com/mirth/connect/donkey/model/message/ResponseMap.java
+++ donkey/src/main/java/com/mirth/connect/donkey/model/message/ResponseMap.java
@@ -9,29 +9,91 @@
 
 package com.mirth.connect.donkey.model.message;
 
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
-public class ResponseMap extends HashMap<String, Object> {
-    
+public class ResponseMap implements Map<String, Object> {
+
+    private Map<String, Object> delegate;
     private Map<String, String> destinationNameMap;
-    
-    public ResponseMap(Map<String, Object> m, Map<String, String> destinationNameMap) {
-        super(m);
+
+    public ResponseMap(Map<String, Object> delegate, Map<String, String> destinationNameMap) {
+        this.delegate = delegate;
         this.destinationNameMap = destinationNameMap;
     }
-    
+
+    @Override
+    public void clear() {
+        delegate.clear();
+    }
+
     @Override
     public boolean containsKey(Object key) {
-        return super.containsKey(key) || (destinationNameMap != null && destinationNameMap.containsKey(key) && super.containsKey(destinationNameMap.get(key)));
+        return delegate.containsKey(key) || (destinationNameMap != null && destinationNameMap.containsKey(key) && delegate.containsKey(destinationNameMap.get(key)));
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return delegate.containsValue(value);
+    }
+
+    @Override
+    public Set<Map.Entry<String, Object>> entrySet() {
+        return delegate.entrySet();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return delegate.equals(o);
     }
 
     @Override
     public Object get(Object key) {
-        Object value = super.get(key);
+        Object value = delegate.get(key);
         if (value == null && destinationNameMap != null && destinationNameMap.containsKey(key)) {
-            value = super.get(destinationNameMap.get(key));
+            value = delegate.get(destinationNameMap.get(key));
         }
         return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return delegate.keySet();
+    }
+
+    @Override
+    public Object put(String key, Object value) {
+        return delegate.put(key, value);
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends Object> m) {
+        delegate.putAll(m);
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return delegate.remove(key);
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public Collection<Object> values() {
+        return delegate.values();
     }
 }
