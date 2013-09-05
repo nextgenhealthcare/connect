35ce3657badaaee4992afe424d03fe71acb6151a
Jacob Brauer <jbrauer@nextgen.com>
2013-09-05 19:34:22 +0000

    Added package-info.java classes for javadoc descriptions of the two userutil packages. Issue: MIRTH-2849

---
+++ server/src/com/mirth/connect/userutil/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Mirth Corporation. All rights reserved.
+ * http://www.mirthcorp.com
+ * 
+ * The software in this package is published under the terms of the MPL
+ * license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+
+/**
+ * This package is included in the JavaScript scope on both the client and the server.&nbsp;Classes
+ * in this package are part of the supported User API for use in channels/scripts.&nbsp;Reference to
+ * any class in Mirth Connect outside of the userutil packages is unsupported.
+ */
+package com.mirth.connect.userutil;
