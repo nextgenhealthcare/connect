c59117204309dab655111328820042c1e89b3609
Jacob Brauer <jbrauer@nextgen.com>
2013-09-05 21:12:54 +0000

    Updated the license header to work with the updated formatter and re-appended it to all files.

--- client/src/com/mirth/connect/client/ui/editors/filter/FilterPane.java
+++ client/src/com/mirth/connect/client/ui/editors/filter/FilterPane.java
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) Mirth Corporation. All rights reserved.
+ * 
  * http://www.mirthcorp.com
  * 
- * The software in this package is published under the terms of the MPL
- * license a copy of which has been included with this distribution in
- * the LICENSE.txt file.
+ * The software in this package is published under the terms of the MPL license a copy of which has
+ * been included with this distribution in the LICENSE.txt file.
  */
 
 package com.mirth.connect.client.ui.editors.filter;
