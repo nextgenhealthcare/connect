1e000f9aace6d0edaacc2f0d5ae390ddd52d07ea
Jacob Brauer <jbrauer@nextgen.com>
2013-10-02 20:12:13 +0000

    Created 2.2.3 tag

---
+++ command/src/com/mirth/connect/cli/launcher/ManifestDirectory.java
@@ -0,0 +1,22 @@
+package com.mirth.connect.cli.launcher;
+
+public class ManifestDirectory implements ManifestEntry {
+    private String dir;
+    private String[] excludes = new String[0];
+
+    public ManifestDirectory(String dir) {
+        this.dir = dir;
+    }
+
+    public String getName() {
+        return dir;
+    }
+
+    public String[] getExcludes() {
+        return excludes;
+    }
+
+    public void setExcludes(String[] excludes) {
+        this.excludes = excludes;
+    }
+}
---
+++ command/src/com/mirth/connect/cli/launcher/ManifestEntry.java
@@ -0,0 +1,5 @@
+package com.mirth.connect.cli.launcher;
+
+public interface ManifestEntry {
+    public String getName();
+}
