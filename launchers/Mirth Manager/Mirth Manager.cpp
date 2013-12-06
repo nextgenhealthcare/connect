8a377d9d901afe111d9f91773a08c58118eb6f08
Jacob Brauer <jbrauer@nextgen.com>
2013-12-06 02:28:56 +0000

    Removed the following directories from trunk (none were included in builds): ant - no longer kept up to date with current versions itext - a modified version of itext with bug fixes that is no longer used launchers - windows launchers that are no longer used uima - not updated for 3.0, but remains in the 2.x tags and branches

--- launchers/Mirth Manager/Mirth Manager.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "Mirth Manager.h"
-
-int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpszCmd,int nCmd)
-{
-	WinExec("java -jar mirth-manager.jar", SW_HIDE);		
-	return 0;
-}
--- launchers/Mirth/Mirth.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "Mirth.h"
-
-int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpszCmd,int nCmd)
-{
-	WinExec("java -jar mirth-launcher.jar launcher.xml", SW_HIDE);
-	return 0;
-}
