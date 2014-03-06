33e658426de2bb2be2360f5145a2be0facd93e85
Nick Rupley <nrupley@nextgen.com>
2014-03-06 20:57:52 +0000

    MIRTH-3135: Fixed bug that caused high-volume channels to become unresponsive when multiple queue threads are used.

--- donkey/src/main/java/com/mirth/connect/donkey/util/xstream/XStreamSerializer.java
+++ donkey/src/main/java/com/mirth/connect/donkey/util/xstream/XStreamSerializer.java
@@ -10,9 +10,6 @@
 package com.mirth.connect.donkey.util.xstream;
 
 import java.io.StringReader;
-import java.lang.ref.WeakReference;
-import java.util.Map;
-import java.util.WeakHashMap;
 
 import org.xmlpull.mxp1.MXParser;
 
@@ -26,15 +23,12 @@ import com.mirth.connect.donkey.model.message.Response;
 import com.mirth.connect.donkey.model.message.attachment.Attachment;
 import com.mirth.connect.donkey.util.Serializer;
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.converters.basic.StringConverter;
 import com.thoughtworks.xstream.core.util.HierarchicalStreams;
 import com.thoughtworks.xstream.io.xml.Xpp3Driver;
 import com.thoughtworks.xstream.io.xml.XppReader;
 import com.thoughtworks.xstream.mapper.MapperWrapper;
 
 public class XStreamSerializer implements Serializer {
-    // http://jira.codehaus.org/browse/XSTR-395
-    private static final Map<String, WeakReference<String>> stringCache = new WeakHashMap<String, WeakReference<String>>();
 
     private static final Class<?>[] annotatedClasses = new Class<?>[] {// @formatter:off
         Attachment.class,
@@ -65,7 +59,6 @@ public class XStreamSerializer implements Serializer {
             xstream = new XStream(new Xpp3Driver());
         }
 
-        xstream.registerConverter(new StringConverter(stringCache));
         xstream.registerConverter(new ConcurrentHashMapConverter(xstream.getMapper()));
         xstream.registerConverter(new PropertiesConverter());
         xstream.setMode(XStream.NO_REFERENCES);
