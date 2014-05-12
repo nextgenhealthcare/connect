72be299f147ca3bc092b2fdfe626738965f799d8
Wayne huang <whuang@nextgen.com>
2014-05-12 18:27:27 +0000

    Added V3_0_3 to the Version enum. Added 3.0.3 to the switch statement in ServerMigrator Updated all creation SQL scripts to insert version 3.0.3 Updated mirth-build.properties to pass in 3.0.3 Updated all plugin/connector metadata files to use 3.0.3 for the mirth version Issue: MIRTH-3270

--- server/src/com/mirth/connect/server/migration/ServerMigrator.java
+++ server/src/com/mirth/connect/server/migration/ServerMigrator.java
@@ -164,6 +164,7 @@ public class ServerMigrator extends Migrator {
             case V3_0_0: return new Migrate3_0_0(startingVersion);
             case V3_0_1: return null;
             case V3_0_2: return new Migrate3_0_2();
+            case V3_0_3: return null;
         } // @formatter:on
 
         return null;
