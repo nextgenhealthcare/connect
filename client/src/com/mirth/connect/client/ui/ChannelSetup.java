9a7a37710fd00b46678707c88ebf57220ba2cd0e
Nick Rupley <nrupley@nextgen.com>
2014-07-19 00:32:12 +0000

    MIRTH-3329: Fixed bug that caused connector type decorations to be applied to the destination table even if the source tab is currently selected.

--- client/src/com/mirth/connect/client/ui/ChannelSetup.java
+++ client/src/com/mirth/connect/client/ui/ChannelSetup.java
@@ -752,7 +752,7 @@ public class ChannelSetup extends javax.swing.JPanel {
     }
 
     public void decorateConnectorType(ConnectorTypeDecoration connectorTypeDecoration) {
-        if (connectorTypeDecoration != null && destinationTable.getSelectedRow() >= 0) {
+        if (connectorTypeDecoration != null && channelView.getSelectedComponent() == destination && destinationTable.getSelectedRow() >= 0) {
             ConnectorTypeData connectorTypeData = (ConnectorTypeData) destinationTable.getModel().getValueAt(destinationTable.getSelectedModelIndex(), destinationTable.getColumnModelIndex(CONNECTOR_TYPE_COLUMN_NAME));
             connectorTypeData.setDecoration(connectorTypeDecoration);
             destinationTable.getModel().setValueAt(connectorTypeData, destinationTable.getSelectedModelIndex(), destinationTable.getColumnModelIndex(CONNECTOR_TYPE_COLUMN_NAME));
