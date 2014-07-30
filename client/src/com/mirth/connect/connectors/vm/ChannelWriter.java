389f57609683b579366f078f45e83113fe2afd6e
Jaysen Patton <jpatton@nextgen.com>
2014-07-30 23:11:09 +0000

    Added the ability to use velocity template variables when routing via a channel writer.
    The new channel ID field will display the unique guid for any channel selected using the combo box. Alternatively, the combo box now dynamically updates based off of what's typed into the channel ID field. If the text matches a channel's unique id, the combo box will display the name of the channel, however, if a channel does not exist for the entered text, the combo box will default to "<Channel Not Found>". Also, if a velocity template variable is used or is empty, the combo box will reflect this state as well with a corresponding option, "<Map Variable>" or "<None>".
    
    This feature is achieved by adding a document listener to the underlying field to handle the aforementioned logic. Because these two fields are now coupled, additional logic has been placed to ensure that an infinite loop does not occur when the user types into the field.
    Issue: MIRTH-2961

--- client/src/com/mirth/connect/connectors/vm/ChannelWriter.java
+++ client/src/com/mirth/connect/connectors/vm/ChannelWriter.java
@@ -13,6 +13,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.mirth.connect.client.ui.Frame;
 import com.mirth.connect.client.ui.PlatformUI;
@@ -25,10 +31,62 @@ public class ChannelWriter extends ConnectorSettingsPanel {
 
     private Frame parent;
     private Map<String, String> channelList;
+    private ArrayList<String> channelNameArray;
+    private Boolean channelIdModified = false;
+    private Boolean comboBoxModified = false;
 
     public ChannelWriter() {
         parent = PlatformUI.MIRTH_FRAME;
         initComponents();
+
+        channelIdField.setToolTipText("<html>The destination channel's unique global id.</html>");
+        channelIdField.getDocument().addDocumentListener(new DocumentListener() {
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateField();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateField();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateField();
+            }
+
+        });
+    }
+
+    private void updateField() {
+        try {
+            channelIdModified = true;
+            if (!comboBoxModified) {
+                String fieldEntry = channelIdField.getText();
+                String selection = "";
+
+                if (StringUtils.isBlank(fieldEntry)) {
+                    selection = "<None>";
+                } else if (channelList.containsValue(fieldEntry)) {
+                    for (Entry<String, String> entry : channelList.entrySet()) {
+                        if (entry.getValue().equals(fieldEntry)) {
+                            fieldEntry = entry.getKey();
+                        }
+                    }
+                    selection = fieldEntry;
+                } else if (fieldEntry.contains("$")) {
+                    selection = "<Map Variable>";
+                } else {
+                    selection = "<Channel Not Found>";
+                }
+
+                channelNames.getModel().setSelectedItem(selection);
+            }
+        } finally {
+            channelIdModified = false;
+        }
     }
 
     @Override
@@ -44,7 +102,7 @@ public class ChannelWriter extends ConnectorSettingsPanel {
 
         VmDispatcherProperties properties = new VmDispatcherProperties();
 
-        properties.setChannelId(channelList.get((String) channelNames.getSelectedItem()));
+        properties.setChannelId(StringUtils.isBlank(channelIdField.getText()) ? "none" : channelIdField.getText());
         properties.setChannelTemplate(template.getText());
 
         return properties;
@@ -54,9 +112,9 @@ public class ChannelWriter extends ConnectorSettingsPanel {
     public void setProperties(ConnectorProperties properties) {
         VmDispatcherProperties props = (VmDispatcherProperties) properties;
 
-        ArrayList<String> channelNameArray = new ArrayList<String>();
+        channelNameArray = new ArrayList<String>();
         channelList = new HashMap<String, String>();
-        channelList.put("None", "none");
+        channelList.put("<None>", "none");
 
         String selectedChannelName = "None";
 
@@ -74,12 +132,14 @@ public class ChannelWriter extends ConnectorSettingsPanel {
         Collections.sort(channelNameArray);
 
         // add "None" to the very top of the list.
-        channelNameArray.add(0, "None");
+        channelNameArray.add(0, "<None>");
 
         channelNames.setModel(new javax.swing.DefaultComboBoxModel(channelNameArray.toArray()));
 
         boolean enabled = parent.isSaveEnabled();
 
+        String channelId = props.getChannelId();
+        channelIdField.setText((channelId.equals("none")) ? "" : channelId);
         channelNames.setSelectedItem(selectedChannelName);
         template.setText(props.getChannelTemplate());
 
@@ -113,11 +173,12 @@ public class ChannelWriter extends ConnectorSettingsPanel {
         channelNames = new com.mirth.connect.client.ui.components.MirthComboBox();
         jLabel7 = new javax.swing.JLabel();
         template = new com.mirth.connect.client.ui.components.MirthSyntaxTextArea();
+        channelIdField = new javax.swing.JTextField();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
-        URL.setText("Channel Name:");
+        URL.setText("Channel Id:");
 
         channelNames.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         channelNames.setToolTipText("<html>Select the channel to which messages accepted by this destination's filter should be written,<br> or none to not write the message at all.</html>");
@@ -145,8 +206,10 @@ public class ChannelWriter extends ConnectorSettingsPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(template, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
+                        .addComponent(channelIdField, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(channelNames, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(0, 13, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -154,22 +217,44 @@ public class ChannelWriter extends ConnectorSettingsPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(URL)
-                    .addComponent(channelNames, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(channelNames, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(channelIdField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel7)
-                        .addGap(0, 55, Short.MAX_VALUE))
-                    .addComponent(template, javax.swing.GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE))
+                        .addGap(0, 133, Short.MAX_VALUE))
+                    .addComponent(template, javax.swing.GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void channelNamesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_channelNamesActionPerformed
+        try {
+            comboBoxModified = true;
+            if (!channelIdModified) {
+                String selectedChannelName = channelNames.getSelectedItem().toString();
+                String channelId = null;
+
+                if (selectedChannelName.equals("<None>")) {
+                    channelId = "";
+                } else if (channelNameArray.contains(selectedChannelName)) {
+                    channelId = channelList.get(selectedChannelName);
+                }
+
+                if (channelId != null) {
+                    channelIdField.setText(channelId);
+                }
+            }
+        } finally {
+            comboBoxModified = false;
+        }
     }//GEN-LAST:event_channelNamesActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel URL;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JTextField channelIdField;
     private com.mirth.connect.client.ui.components.MirthComboBox channelNames;
     private javax.swing.JLabel jLabel7;
     private com.mirth.connect.client.ui.components.MirthSyntaxTextArea template;
