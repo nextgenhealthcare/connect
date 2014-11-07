498c3cefc6988440791c287b12d8e419d6838c1f
Nick Rupley <nrupley@nextgen.com>
2014-11-07 00:10:17 +0000

    MIRTH-3493: Added additional information to the response status when an HL7 NACK occurs, which will be available in alert actions. An indicator of rejected/error is included, as well as MSA.3 and ERR.1, if present.

--- server/src/com/mirth/connect/plugins/datatypes/hl7v2/HL7v2ResponseValidator.java
+++ server/src/com/mirth/connect/plugins/datatypes/hl7v2/HL7v2ResponseValidator.java
@@ -52,52 +52,100 @@ public class HL7v2ResponseValidator implements ResponseValidator {
         String responseData = response.getMessage();
 
         if (StringUtils.isNotBlank(responseData)) {
-            if (responseData.trim().startsWith("<")) {
-                // XML response received
-                try {
+            try {
+                if (responseData.trim().startsWith("<")) {
+                    // XML response received
                     Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new CharArrayReader(responseData.toCharArray())));
                     String ackCode = XPathFactory.newInstance().newXPath().compile("//MSA.1/text()").evaluate(doc).trim();
-                    if (Arrays.asList(errorACKCodes).contains(ackCode) || Arrays.asList(rejectedACKCodes).contains(ackCode)) {
-                        handleNACK(response);
+
+                    boolean rejected = Arrays.asList(rejectedACKCodes).contains(ackCode);
+                    boolean error = rejected || Arrays.asList(errorACKCodes).contains(ackCode);
+
+                    if (error || rejected) {
+                        String msa3 = StringUtils.trim(XPathFactory.newInstance().newXPath().compile("//MSA.3/text()").evaluate(doc));
+                        String err1 = StringUtils.trim(XPathFactory.newInstance().newXPath().compile("//ERR.1/text()").evaluate(doc));
+                        handleNACK(response, rejected, msa3, err1);
                     } else if (Arrays.asList(successfulACKCodes).contains(ackCode)) {
                         response.setStatus(Status.SENT);
                     }
-                } catch (Exception e) {
-                    response.setStatus(Status.QUEUED);
-                    response.setStatusMessage("Error validating response: " + e.getMessage());
-                    response.setError(ErrorMessageBuilder.buildErrorMessage(this.getClass().getSimpleName(), response.getStatusMessage(), e));
-                }
-            } else {
-                // ER7 response received
+                } else {
+                    // ER7 response received
+                    if (serializationProperties.isConvertLineBreaks()) {
+                        responseData = StringUtil.convertLineBreaks(responseData, serializationSegmentDelimiter);
+                    }
 
-                if (serializationProperties.isConvertLineBreaks()) {
-                    responseData = StringUtil.convertLineBreaks(responseData, serializationSegmentDelimiter);
-                }
+                    int index = -1;
+                    boolean valid = true;
 
-                int index = -1;
-                boolean valid = true;
+                    // Attempt to find the MSA segment using the segment delimiters in the serialization properties
+                    if ((index = responseData.indexOf(serializationSegmentDelimiter + "MSA")) >= 0) {
+                        // MSA found; add the length of the segment delimiter, MSA, and field separator to get to the index of MSA.1
+                        index += serializationSegmentDelimiter.length() + 4;
 
-                // Attempt to find the MSA segment using the segment delimiters in the serialization properties
-                if ((index = responseData.indexOf(serializationSegmentDelimiter + "MSA")) >= 0) {
-                    // MSA found; add the length of the segment delimiter, MSA, and field separator to get to the index of MSA.1
-                    index += serializationSegmentDelimiter.length() + 4;
+                        if (index < responseData.length()) {
+                            boolean rejected = startsWithAny(responseData, rejectedACKCodes, index);
+                            boolean error = rejected || startsWithAny(responseData, errorACKCodes, index);
 
-                    if (startsWithAny(responseData, errorACKCodes, index) || startsWithAny(responseData, rejectedACKCodes, index)) {
-                        handleNACK(response);
-                    } else if (startsWithAny(responseData, successfulACKCodes, index)) {
-                        response.setStatus(Status.SENT);
+                            if (error || rejected) {
+                                String msa3 = null;
+                                String err1 = null;
+                                char fieldSeparator = responseData.charAt(index - 1);
+
+                                // Index of MSA.2
+                                index = responseData.indexOf(fieldSeparator, index);
+                                if (index >= 0) {
+                                    // Index of MSA.3
+                                    index = responseData.indexOf(fieldSeparator, index + 1);
+                                    if (index >= 0) {
+                                        // Find the next index of either the field separator or segment delimiter, and then the resulting substring
+                                        String tempSegment = StringUtils.substring(responseData, index + 1);
+                                        index = StringUtils.indexOfAny(tempSegment, fieldSeparator + serializationSegmentDelimiter);
+
+                                        if (index >= 0) {
+                                            msa3 = StringUtils.substring(tempSegment, 0, index);
+                                        } else {
+                                            msa3 = StringUtils.substring(tempSegment, 0);
+                                        }
+                                    }
+                                }
+
+                                if ((index = responseData.indexOf(serializationSegmentDelimiter + "ERR")) >= 0) {
+                                    // ERR found; add the length of the segment delimiter, ERR, and field separator to get to the index of ERR.1
+                                    index += serializationSegmentDelimiter.length() + 4;
+                                    // Find the next index of either the field separator or segment delimiter, and then the resulting substring
+                                    String tempSegment = StringUtils.substring(responseData, index);
+                                    index = StringUtils.indexOfAny(tempSegment, fieldSeparator + serializationSegmentDelimiter);
+
+                                    if (index >= 0) {
+                                        err1 = StringUtils.substring(tempSegment, 0, index);
+                                    } else {
+                                        err1 = StringUtils.substring(tempSegment, 0);
+                                    }
+                                }
+
+                                handleNACK(response, rejected, msa3, err1);
+                            } else if (startsWithAny(responseData, successfulACKCodes, index)) {
+                                response.setStatus(Status.SENT);
+                            } else {
+                                valid = false;
+                            }
+                        } else {
+                            valid = false;
+                        }
                     } else {
                         valid = false;
                     }
-                } else {
-                    valid = false;
-                }
 
-                if (!valid) {
-                    response.setStatus(Status.QUEUED);
-                    response.setStatusMessage("Invalid HL7 v2.x acknowledgement received.");
-                    response.setError(response.getStatusMessage());
+                    if (!valid) {
+                        response.setStatus(Status.QUEUED);
+                        response.setStatusMessage("Invalid HL7 v2.x acknowledgement received.");
+                        response.setError(response.getStatusMessage());
+                    }
                 }
+            } catch (Exception e) {
+                response.setStatus(Status.QUEUED);
+                response.setStatusMessage("Error validating response: " + e.getMessage());
+                response.setError(ErrorMessageBuilder.buildErrorMessage(this.getClass().getSimpleName(), response.getStatusMessage(), e));
             }
         } else {
             response.setStatus(Status.QUEUED);
@@ -127,9 +175,25 @@ public class HL7v2ResponseValidator implements ResponseValidator {
         return props;
     }
 
-    private void handleNACK(Response response) {
+    private void handleNACK(Response response, boolean rejected, String msa3, String err1) {
         response.setStatus(Status.ERROR);
-        response.setStatusMessage("NACK sent from receiver.");
-        response.setError("NACK sent from receiver: " + response.getMessage());
+
+        StringBuilder nackMessage = new StringBuilder("NACK sent from receiver. (");
+        nackMessage.append(rejected ? "Rejected)" : "Error)");
+
+        if (StringUtils.isNotEmpty(msa3)) {
+            nackMessage.append('\n');
+            nackMessage.append(msa3);
+        }
+        if (StringUtils.isNotEmpty(err1)) {
+            nackMessage.append('\n');
+            nackMessage.append(err1);
+        }
+
+        response.setStatusMessage(nackMessage.toString());
+
+        nackMessage.append("\n\n");
+        nackMessage.append(response.getMessage());
+        response.setError(nackMessage.toString());
     }
 }
