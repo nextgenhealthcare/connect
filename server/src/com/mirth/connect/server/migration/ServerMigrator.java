0b7905be7072b44fdc67fdbb190aabff9e81d6b2
Nick Rupley <nrupley@nextgen.com>
2015-04-27 14:23:36 +0000

    MIRTH-3702: Added 3.2.2 to Version and ServerMigrator, updated SQL creation scripts, mirth-build.properties, and all extension metadata XML files.

--- server/src/com/mirth/connect/server/migration/ServerMigrator.java
+++ server/src/com/mirth/connect/server/migration/ServerMigrator.java
@@ -170,6 +170,7 @@ public class ServerMigrator extends Migrator {
             case V3_1_1: return new Migrate3_1_1();
             case V3_2_0: return new Migrate3_2_0();
             case V3_2_1: return null;
+            case V3_2_2: return null;
         } // @formatter:on
 
         return null;
