eafd4b2d7c28c4e8e7b94437516a6823171eefd6
Nick Rupley <nrupley@nextgen.com>
2016-08-24 16:28:49 +0000

    MIRTH-4031: Added 3.4.2 to Version and ServerMigrator in trunk.

--- server/src/com/mirth/connect/server/migration/ServerMigrator.java
+++ server/src/com/mirth/connect/server/migration/ServerMigrator.java
@@ -212,6 +212,7 @@ public class ServerMigrator extends Migrator {
             case V3_3_2: return null;
             case V3_4_0: return new Migrate3_4_0();
             case V3_4_1: return null;
+            case V3_4_2: return null;
             case V3_5_0: return null;
         } // @formatter:on
 
