77b649df30c94f2b4af356ed4826031e4bd90cf8
Kevin Hsu <khsu@nextgen.com>
2017-10-25 17:12:26 -0700

    Add comparison method for ConnectionStatusEventType so clustering can determine what state to show when cluster results are mixed.

--- donkey/src/main/java/com/mirth/connect/donkey/model/event/ConnectionStatusEventType.java
+++ donkey/src/main/java/com/mirth/connect/donkey/model/event/ConnectionStatusEventType.java
@@ -20,6 +20,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 public enum ConnectionStatusEventType {
     IDLE(true), READING(true), WRITING(true), POLLING(true), RECEIVING(true), SENDING(true), WAITING_FOR_RESPONSE(true), CONNECTED(true), CONNECTING(true), DISCONNECTED(false), INFO(false), FAILURE(false);
     
+	private static ConnectionStatusEventType[] priority = new ConnectionStatusEventType[] {SENDING, RECEIVING, WRITING, READING, POLLING, CONNECTED, WAITING_FOR_RESPONSE, CONNECTING, IDLE};
     private boolean state;
     
     private ConnectionStatusEventType(boolean state) {
@@ -45,5 +46,19 @@ public enum ConnectionStatusEventType {
     @Override
     public String toString() {
         return WordUtils.capitalizeFully(super.toString().replace("_", " "));
+    }
+    
+    static public ConnectionStatusEventType compare(ConnectionStatusEventType status1, ConnectionStatusEventType status2) {
+		if (status1 == null && status2 != null) return status2;
+		else if (status2 == null && status1 != null) return status1;
+		else if (status1.equals(status2)) return status1;
+		
+		for (ConnectionStatusEventType statusType : priority) {
+			if (statusType.equals(status1) || statusType.equals(status2)) {
+				return statusType;
+			}
+		}
+		
+		return ConnectionStatusEventType.IDLE;
     }
 }
