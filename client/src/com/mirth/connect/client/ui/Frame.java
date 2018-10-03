432efca6b489ed61c4b6f59d80dd969719d71b95
Nick Rupley <nrupley@nextgen.com>
2018-10-03 09:49:09 -0700

    Fixing NPE that occurred when the resources tab isn't available.

--- client/src/com/mirth/connect/client/ui/Frame.java
+++ client/src/com/mirth/connect/client/ui/Frame.java
@@ -4478,12 +4478,18 @@ public class Frame extends JXFrame {
         if (settingsPane == null) {
             settingsPane = new SettingsPane();
         }
+
+        List<ResourceProperties> resourceProperties = null;
+
         SettingsPanelResources resourcesPanel = (SettingsPanelResources) settingsPane.getSettingsPanel(SettingsPanelResources.TAB_NAME);
-        List<ResourceProperties> resourceProperties = resourcesPanel.getCachedResources();
-        if (resourceProperties == null) {
-            resourcesPanel.refresh();
+        if (resourcesPanel != null) {
             resourceProperties = resourcesPanel.getCachedResources();
+            if (resourceProperties == null) {
+                resourcesPanel.refresh();
+                resourceProperties = resourcesPanel.getCachedResources();
+            }
         }
+
         return resourceProperties;
     }
 
