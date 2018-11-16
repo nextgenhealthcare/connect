555638980f93db8879e998804f1bf341cdcfaef3
Nick Rupley <nrupley@nextgen.com>
2018-11-16 13:00:28 -0800

    Updating to 3.6.2.

--- server/src/com/mirth/connect/server/migration/ServerMigrator.java
+++ server/src/com/mirth/connect/server/migration/ServerMigrator.java
@@ -219,6 +219,7 @@ public class ServerMigrator extends Migrator {
             case V3_5_2: return null;
             case V3_6_0: return null;
             case V3_6_1: return null;
+            case V3_6_2: return null;
         } // @formatter:on
 
         return null;
