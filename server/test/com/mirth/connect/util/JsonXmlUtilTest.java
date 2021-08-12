be3c11198736af6ec6330aeb26bdb0e284dffeb2
Peter Ladesma <pladesma@nextgen.com>
2021-08-12 15:50:27 -0700

    ROCKSOLID-4470 Adds json->xml tests, particularly for the case when the JSON has attributes that come after the properties.

--- server/test/com/mirth/connect/util/JsonXmlUtilTest.java
+++ server/test/com/mirth/connect/util/JsonXmlUtilTest.java
@@ -13,7 +13,7 @@ public class JsonXmlUtilTest {
 	@Test
 	public void testXmlToJson() {
 		try {
-			JsonXmlUtil.xmlToJson(validXml);
+			JsonXmlUtil.xmlToJson(VALID_XML);
 		} catch (Exception e) {
 			fail("Failed to convert valid XML to JSON. Exception: " + e.getMessage());
 		}
@@ -23,7 +23,7 @@ public class JsonXmlUtilTest {
 	public void testXmlToJsonWithExternalDTD() {
 		boolean exceptionCaught = false;
 		try {
-			JsonXmlUtil.xmlToJson(xmlWithExternalDtd);
+			JsonXmlUtil.xmlToJson(XML_WITH_EXTERNAL_DTD);
 		} catch (Exception e) {
 			assertEquals(TransformerException.class, e.getClass());
 			exceptionCaught = true;
@@ -31,11 +31,39 @@ public class JsonXmlUtilTest {
 		assertTrue(exceptionCaught);
 	}
 	
-	private static String validXml =  "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n"
+	@Test
+	public void testJsonToXml() throws Exception {
+		assertEquals(EXPECTED_XML_FROM_JSON_1, JsonXmlUtil.jsonToXml(JSON_1));
+	}
+	
+	@Test
+	public void testJsonWithAttrsAtEndToXml() throws Exception {
+		assertEquals(EXPECTED_XML_FROM_JSON_1, JsonXmlUtil.jsonToXml(JSON_1_WITH_ATTRS_AT_END));
+	}
+	
+	private static String VALID_XML =  "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n"
 			+ "<foo><bar>bar</bar></foo>";
 	
-	private static String xmlWithExternalDtd =  "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n"
+	private static String XML_WITH_EXTERNAL_DTD =  "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n"
 			+ "<!DOCTYPE foo [\r\n"
 			+ "<!ELEMENT foo ANY >\r\n"
 			+ "<!ENTITY xxe SYSTEM \"file:///dev/random\" >]><foo>&xxe;</foo>";
+	
+	private static String JSON_1 = "{\n" + 
+			"	\"key1\": {\n" + 
+			"		\"@attr1\": \"some attribute\",\n" + 
+			"		\"@attr2\": \"some attribute 2\",\n" + 
+			"		\"prop1\": \"some property\"\n" + 
+			"	}\n" + 
+			"}";
+	
+	private static String JSON_1_WITH_ATTRS_AT_END = "{\n" + 
+			"	\"key1\": {\n" + 
+			"		\"@attr1\": \"some attribute\",\n" + 
+			"		\"prop1\": \"some property\"\n" + 
+			"		\"@attr2\": \"some attribute 2\",\n" + 
+			"	}\n" + 
+			"}";
+	
+	private static String EXPECTED_XML_FROM_JSON_1 = "<?xml version='1.0' encoding='UTF-8'?><key1 attr1=\"some attribute\" attr2=\"some attribute 2\"><prop1>some property</prop1></key1>";
 }
