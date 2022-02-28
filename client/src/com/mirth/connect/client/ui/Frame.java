d15d4598f4148507317e5898b218e79f91d38de6
Lisa Miller <lmiller@nextgen.com>
2022-02-28 08:09:12 -0600

    ROCKSOLID-7156 Add Inactivity Listener

--- client/src/com/mirth/connect/client/ui/Frame.java
+++ client/src/com/mirth/connect/client/ui/Frame.java
@@ -9,6 +9,7 @@
 
 package com.mirth.connect.client.ui;
 
+import java.awt.AWTEvent;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -20,6 +21,9 @@ import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.Toolkit;
 import java.awt.Window;
+import java.awt.event.AWTEventListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.InputEvent;
@@ -51,6 +55,7 @@ import java.util.prefs.Preferences;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -65,6 +70,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
+import javax.swing.Timer;
 import javax.swing.border.LineBorder;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -126,11 +132,14 @@ import com.mirth.connect.model.ConnectorMetaData;
 import com.mirth.connect.model.DashboardChannelInfo;
 import com.mirth.connect.model.DashboardStatus;
 import com.mirth.connect.model.DashboardStatus.StatusType;
+import com.mirth.connect.model.ServerEvent.Level;
 import com.mirth.connect.model.EncryptionSettings;
 import com.mirth.connect.model.InvalidChannel;
 import com.mirth.connect.model.MetaData;
 import com.mirth.connect.model.PluginMetaData;
+import com.mirth.connect.model.PublicServerSettings;
 import com.mirth.connect.model.ResourceProperties;
+import com.mirth.connect.model.ServerEvent;
 import com.mirth.connect.model.ServerSettings;
 import com.mirth.connect.model.UpdateSettings;
 import com.mirth.connect.model.User;
@@ -147,6 +156,7 @@ import com.mirth.connect.util.CharsetUtils;
 import com.mirth.connect.util.DirectedAcyclicGraphNode;
 import com.mirth.connect.util.JavaScriptSharedUtil;
 import com.mirth.connect.util.MigrationUtil;
+
 import javafx.application.Platform;
 
 /**
@@ -403,6 +413,120 @@ public class Frame extends JXFrame {
         }
     }
 
+    class InactivityListener implements ActionListener, AWTEventListener
+    {
+        public final static long KEY_EVENTS = AWTEvent.KEY_EVENT_MASK;
+
+        public final static long MOUSE_EVENTS =
+            AWTEvent.MOUSE_MOTION_EVENT_MASK + AWTEvent.MOUSE_EVENT_MASK;
+
+        public final static long USER_EVENTS = KEY_EVENTS + MOUSE_EVENTS;
+
+        private Window window;
+        private Action action;
+        private int interval;
+        private long eventMask;
+        private Timer timer = new Timer(0, this);
+
+        /*
+         *  Use a default inactivity interval of 1 minute and listen for
+         *  USER_EVENTS
+         */
+        public InactivityListener(Window window, Action action)
+        {
+            this(window, action, 1);
+        }
+
+        /*
+         *  Specify the inactivity interval and listen for USER_EVENTS
+         */
+        public InactivityListener(Window window, Action action, int interval)
+        {
+            this(window, action, interval, USER_EVENTS);
+        }
+
+        /*
+         *  Specify the inactivity interval and the events to listen for
+         */
+        public InactivityListener(Window window, Action action, int minutes, long eventMask)
+        {
+            this.window = window;
+            setAction( action );
+            setInterval( minutes );
+            setEventMask( eventMask );
+        }
+
+        /*
+         *  The Action to be invoked after the specified inactivity period
+         */
+        public void setAction(Action action)
+        {
+            this.action = action;
+        }
+
+        /*
+         *  The interval before the Action is invoked specified in minutes
+         */
+        public void setInterval(int minutes)
+        {
+            setIntervalInMillis(minutes * 60000);
+        }
+
+        /*
+         *  The interval before the Action is invoked specified in milliseconds
+         */
+        public void setIntervalInMillis(int interval)
+        {
+            this.interval = interval;
+            timer.setInitialDelay(interval);
+        }
+
+        /*
+         *  A mask specifying the events to be passed to the AWTEventListener
+         */
+        public void setEventMask(long eventMask)
+        {
+            this.eventMask = eventMask;
+        }
+
+        /*
+         *  Start listening for events.
+         */
+        public void start()
+        {
+            timer.setInitialDelay(interval);
+            timer.setRepeats(false);
+            timer.start();
+            Toolkit.getDefaultToolkit().addAWTEventListener(this, eventMask);
+        }
+
+        /*
+         *  Stop listening for events
+         */
+        public void stop()
+        {
+            Toolkit.getDefaultToolkit().removeAWTEventListener(this);
+            timer.stop();
+        }
+
+        //  Implement ActionListener for the Timer
+
+        public void actionPerformed(ActionEvent e)
+        {
+            ActionEvent ae = new ActionEvent(window, ActionEvent.ACTION_PERFORMED, "");
+            action.actionPerformed(ae);
+        }
+
+        //  Implement AWTEventListener
+
+        public void eventDispatched(AWTEvent e)
+        {
+            if (timer.isRunning()) {
+                timer.restart();
+            }
+        }
+    }
+    
     /**
      * Called to set up this main window frame.
      */
@@ -520,7 +644,7 @@ public class Frame extends JXFrame {
             alertError(this, "Could not get Rhino language version.");
         }
 
-        // Display the server timezone information
+        // Display the server time zone information
         statusBar.setTimezoneText(PlatformUI.SERVER_TIMEZONE);
         statusBar.setServerTime(PlatformUI.SERVER_TIME);
 
@@ -556,6 +680,19 @@ public class Frame extends JXFrame {
 
         LicenseClient.start();
 
+        AbstractAction logout = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                logout(false, false);
+            }
+        };
+        
+        // Fetch log timeout from server and use it for inactivity timer
+        PublicServerSettings publicServerSettings = mirthClient.getPublicServerSettings();
+        if (publicServerSettings.getAdministratorAutoLogoutIntervalEnabled() == true) {
+        	InactivityListener listener = new InactivityListener(this, logout, Integer.parseInt(publicServerSettings.getAdministratorAutoLogoutIntervalField()));
+        	listener.start();
+        }
+        
         // DEBUGGING THE UIDefaults:
 
 //         UIDefaults uiDefaults = UIManager.getDefaults(); Enumeration enum1 =
@@ -2049,10 +2186,17 @@ public class Frame extends JXFrame {
     }
 
     public boolean logout(boolean quit) {
-        if (!confirmLeave()) {
+        return logout(quit, true);
+    }
+    
+    public boolean logout(boolean quit, boolean confirmFirst) {
+        if (confirmFirst && !confirmLeave()) {
             return false;
         }
-
+        Boolean inactivity = false;
+        if (!quit && !confirmFirst) {
+        	inactivity = true;
+        }
         LicenseClient.stop();
 
         // MIRTH-3074 Remove the keyEventDispatcher to prevent memory leak.
