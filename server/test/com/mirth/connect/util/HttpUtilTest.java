e13ce19bb00e2ff852d7a161a0a83b2609967d7b
Nick Rupley <NRupley@nextgen.com>
2022-03-03 12:53:01 -0800

    Adding SSLContext so that it can be overridden if needed.

--- server/test/com/mirth/connect/util/HttpUtilTest.java
+++ server/test/com/mirth/connect/util/HttpUtilTest.java
@@ -14,19 +14,25 @@ import static org.junit.Assert.fail;
 
 import java.util.UUID;
 
+import javax.net.ssl.SSLContext;
+
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.ssl.SSLContexts;
 import org.junit.Test;
 
 public class HttpUtilTest {
 
     @Test
     public void testExecuteGetRequest() throws Exception {
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(new TrustAllStrategy()).build();
+
         // Test GET request to real website
-        assertFalse(StringUtils.isBlank(HttpUtil.doExecuteGetRequest("https://www.nextgen.com", 30000, true, MirthSSLUtil.DEFAULT_HTTPS_CLIENT_PROTOCOLS, MirthSSLUtil.DEFAULT_HTTPS_CIPHER_SUITES)));
+        assertFalse(StringUtils.isBlank(HttpUtil.doExecuteGetRequest("https://www.nextgen.com", 30000, true, MirthSSLUtil.DEFAULT_HTTPS_CLIENT_PROTOCOLS, MirthSSLUtil.DEFAULT_HTTPS_CIPHER_SUITES, sslContext)));
 
         // Test GET request to fake website
         try {
-            HttpUtil.doExecuteGetRequest("https://www." + UUID.randomUUID().toString() + UUID.randomUUID().toString() + ".com", 30000, true, MirthSSLUtil.DEFAULT_HTTPS_CLIENT_PROTOCOLS, MirthSSLUtil.DEFAULT_HTTPS_CIPHER_SUITES);
+            HttpUtil.doExecuteGetRequest("https://www." + UUID.randomUUID().toString() + UUID.randomUUID().toString() + ".com", 30000, true, MirthSSLUtil.DEFAULT_HTTPS_CLIENT_PROTOCOLS, MirthSSLUtil.DEFAULT_HTTPS_CIPHER_SUITES, sslContext);
             fail("Exception should have been thrown");
         } catch (Exception e) {
         }
