9fdfeceff8a7c22ddf05dabcd403850d28639503
Jonathan Do <jdo@nextgen.com>
2022-04-05 12:59:44 -0700

    [ROCKSOLID-6172] Add code to sort Filter and Transformer steps by sequenceNumber

---
+++ server/src/com/mirth/connect/model/converters/FilterTransformerElementsConverter.java
@@ -0,0 +1,68 @@
+package com.mirth.connect.model.converters;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import com.mirth.connect.model.FilterTransformerElement;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.converters.collections.CollectionConverter;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.mapper.Mapper;
+
+import edu.emory.mathcs.backport.java.util.Collections;
+
+public class FilterTransformerElementsConverter extends CollectionConverter {
+
+    public FilterTransformerElementsConverter(Mapper mapper) {
+        super(mapper);
+    }
+    
+    @Override
+    public boolean canConvert(Class type) {
+        return type.equals(ArrayList.class);
+    }
+    
+    @Override
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+        List list = (List) super.unmarshal(reader, context);
+        
+        if (list != null && !list.isEmpty()) {
+            Object firstElement = list.get(0);
+            
+            if (FilterTransformerElement.class.isAssignableFrom(firstElement.getClass())) {
+                Collections.sort(list, new Comparator<FilterTransformerElement>() {
+                    public int compare(FilterTransformerElement e1, FilterTransformerElement e2) {
+                        String sequenceNumber1 = e1.getSequenceNumber();
+                        String sequenceNumber2 = e2.getSequenceNumber();
+                        
+                        // if sequenceNumberN is a child of an iterator step (i.e., "n-n") convert to "n.n" for comparison
+                        if (sequenceNumber1.contains("-")) {
+                            sequenceNumber1 = sequenceNumber1.replace("-", ".");
+                        }
+                        if (sequenceNumber2.contains("-")) {
+                            sequenceNumber2 = sequenceNumber2.replace("-", ".");
+                        }
+                        
+                        Double convertedSequenceNumber1 = Double.parseDouble(sequenceNumber1);
+                        Double convertedSequenceNumber2 = Double.parseDouble(sequenceNumber2);
+                        Double difference = convertedSequenceNumber1 - convertedSequenceNumber2;
+                        
+                        // -1 = convertedSequenceNumber1 < convertedSequenceNumber2
+                        //  0 = convertedSequenceNumber1 == convertedSequenceNumber2
+                        //  1 = convertedSequenceNumber1 > convertedSequenceNumber2
+                        if (difference < 0) {
+                            return -1;
+                        } else if (difference == 0) {
+                            return 0;
+                        } else {
+                            return 1;
+                        }
+                    }
+                });
+            }
+        }
+        
+        return list;
+    }
+}
