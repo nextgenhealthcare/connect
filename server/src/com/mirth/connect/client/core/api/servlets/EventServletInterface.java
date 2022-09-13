328e6c3e0b2d4abd1af3ce628995c4383e76ec07
jdo <jdo@nextgen.com>
2022-09-13 16:09:14 -0700

    [ROCKSOLID-9215] Remove unused import statement

--- server/src/com/mirth/connect/client/core/api/servlets/EventServletInterface.java
+++ server/src/com/mirth/connect/client/core/api/servlets/EventServletInterface.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
