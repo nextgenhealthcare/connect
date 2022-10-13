09416d50ff17e9f5867af6f828949ad10a1c7173
jdo <jdo@nextgen.com>
2022-10-13 11:29:47 -0700

    [ROCKSOLID-9372] Add skeletons for hash methods

--- server/src/com/mirth/connect/server/userutil/HashUtil.java
+++ server/src/com/mirth/connect/server/userutil/HashUtil.java
@@ -1,5 +1,16 @@
 package com.mirth.connect.server.userutil;
 
-public class HashUtil {
+import java.security.MessageDigest;
 
+/**
+ * Provides hash utility methods.
+ */
+public class HashUtil {
+    private HashUtil() {}
+    
+    public static String generate(Object data) { return null; }
+    
+    public static String generate(byte[] bytes, String algorithm) { return null; }
+    
+    public static String generate(String str, String encoding, String algorithm) { return null; }
 }
