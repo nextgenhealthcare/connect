07a9c75bbecb0b66e6e87cc2669a692d4fe6f828
jdo <jdo@nextgen.com>
2022-10-13 13:13:18 -0700

    [ROCKSOLID-9372] Add code to generate HashUtil methods to allow generating of hash

--- server/src/com/mirth/connect/server/userutil/HashUtil.java
+++ server/src/com/mirth/connect/server/userutil/HashUtil.java
@@ -1,16 +1,72 @@
 package com.mirth.connect.server.userutil;
 
+import java.nio.charset.Charset;
 import java.security.MessageDigest;
+import java.util.Base64;
 
 /**
  * Provides hash utility methods.
  */
 public class HashUtil {
+    private static final String DEFAULT_ALGORITHM = "SHA-256";
+    
     private HashUtil() {}
     
-    public static String generate(Object data) { return null; }
+    /**
+     * Takes in any object and generates a SHA-256 hash.
+     * @param data
+     *          The data to hash.
+     * @return hash
+     *          The generated SHA-256 hash of the data.
+     * @throws Exception
+     *          If generating a SHA-256 hash fails.
+     */
+    public static String generate(Object data) throws Exception { 
+        if (data instanceof byte[]) {
+            return generate((byte[]) data, Charset.defaultCharset(), DEFAULT_ALGORITHM);
+        } else {
+            return generate(String.valueOf(data).getBytes(), Charset.defaultCharset(), DEFAULT_ALGORITHM); 
+        }
+    }
     
-    public static String generate(byte[] bytes, String algorithm) { return null; }
+    /**
+     * Takes in a string, an encoding, and a hashing algorithm and generates a hash.
+     * @param str
+     *          The string to hash.
+     * @param encoding
+     *          The character encoding to use.
+     * @param algorithm
+     *          The hashing algorithm to use.
+     * @return hash
+     *          The generated hash of the string.
+     * @throws Exception
+     *          If generating a hash of the string fails.
+     */
+    public static String generate(String str, Charset encoding, String algorithm) throws Exception { 
+        return generate(str.getBytes(encoding), encoding, algorithm); 
+    }
+
     
-    public static String generate(String str, String encoding, String algorithm) { return null; }
+    /**
+     * Takes in a byte[], an encoding, and a hashing algorithm and generates a hash.
+     * @param bytes
+     *          The byte[] to hash.
+     * @param encoding
+     *          The character encoding to use.
+     * @param algorithm
+     *          The hashing algorithm to use.
+     * @return hash
+     *          The generated hash of the byte[].
+     * @throws Exception
+     *          If generating a hash of the byte[] fails.
+     */
+    public static String generate(byte[] bytes, Charset encoding, String algorithm) throws Exception { 
+        MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+        
+        messageDigest.update(bytes);
+        byte[] hash = messageDigest.digest();
+        
+        //return Base64.getEncoder().encodeToString(hash);
+        return hash.toString();
+    }
 }
